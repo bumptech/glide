5a82c4f602293960de89aa1065b2e4cfb84aafb3
Sam Judd <sam@bu.mp>
2013-01-09 18:33:08 -0800

    Add size based image downloading
    Download smallest size available that is still
    larger than our target area.

--- samples/flickr/src/com/bumptech/flickr/api/Api.java
+++ samples/flickr/src/com/bumptech/flickr/api/Api.java
@@ -8,7 +8,9 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,8 +22,32 @@ import java.util.List;
 public class Api {
     private static final String API_KEY = "f0e6fbb5fdf1f3842294a1d21f84e8a6";
     private static final String SIGNED_API_URL = "http://api.flickr.com/services/rest/?method=%s&format=json&api_key=" + API_KEY;
-    private static final String PHOTO_URL = "http://farm%s.staticflickr.com/%s/%s_%s.jpg";
+    private static final String PHOTO_URL = "http://farm%s.staticflickr.com/%s/%s_%s_%s.jpg";
     private final Downloader downloader;
+
+    private static final Map<Integer, String> EDGE_TO_SIZE_KEY = new HashMap<Integer, String>() {{
+        put(75, "s");
+        put(100, "t");
+        put(150, "q");
+        put(240, "m");
+        put(320, "n");
+        put(500, "-");
+        put(640, "z");
+        put(1024, "b");
+        put(Integer.MAX_VALUE, "o");
+    }};
+
+    private static String getSizeKey(int width, int height) {
+        final int largestEdge = width > height ? width : height;
+
+        final String result = EDGE_TO_SIZE_KEY.get(Integer.MAX_VALUE);
+        for (int edge : EDGE_TO_SIZE_KEY.keySet()) {
+            if (largestEdge <= edge) {
+                return EDGE_TO_SIZE_KEY.get(edge);
+            }
+        }
+        return result;
+    }
 
     public interface SearchCallback {
         public void onSearchCompleted(List<Photo> photos);
@@ -43,8 +69,9 @@ public class Api {
         return getUrlForMethod("flickr.photos.search") + "&text=" + text + "&per_page=500";
     }
 
-    private static String getPhotoUrl(Photo photo) {
-        return String.format(PHOTO_URL, photo.farm, photo.server, photo.id, photo.secret);
+    private static String getPhotoUrl(Photo photo, int width, int height) {
+        String sizeKey = getSizeKey(width, height);
+        return String.format(PHOTO_URL, photo.farm, photo.server, photo.id, photo.secret, sizeKey);
     }
 
     public void search(String text, final SearchCallback cb) {
@@ -71,13 +98,13 @@ public class Api {
         });
     }
 
-    public void downloadPhoto(Photo photo, File cacheDir, final PhotoCallback cb) {
-        File out = new File(cacheDir.getPath() + File.separator + photo.id + photo.secret);
+    public void downloadPhoto(Photo photo, int width, int height, File cacheDir, final PhotoCallback cb) {
+        File out = new File(cacheDir.getPath() + File.separator + photo.id + photo.secret + width + height);
         if (out.exists()) {
             cb.onDownloadComplete(out.getPath());
         } else {
             Log.d("API: missing photo, downloading");
-            downloader.download(getPhotoUrl(photo), out, new Downloader.DiskCallback() {
+            downloader.download(getPhotoUrl(photo, width, height), out, new Downloader.DiskCallback() {
                 @Override
                 public void onDownloadReady(String path) {
                     cb.onDownloadComplete(path);
