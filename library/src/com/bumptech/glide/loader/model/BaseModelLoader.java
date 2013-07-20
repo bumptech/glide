78d7c407e08dd6167fa8d3a3f8bc4b435a841ca0
Sam Judd <sam@bu.mp>
2013-07-20 08:12:03 -0700

    Add a BaseModelLoader
    Allows us to remove the repetitive
    ModelLoader.close() -> StreamOpener.cancel()

---
+++ library/src/com/bumptech/glide/loader/model/BaseModelLoader.java
@@ -0,0 +1,27 @@
+package com.bumptech.glide.loader.model;
+
+import com.bumptech.glide.loader.stream.StreamLoader;
+
+/**
+ * A base class that handles canceling any existing {@link StreamLoader} when
+ * cleared
+ */
+public abstract class BaseModelLoader<T> implements ModelLoader<T> {
+    private StreamLoader current;
+
+    @Override
+    public final StreamLoader getStreamOpener(T model, int width, int height) {
+        current = buildStreamOpener(model, width, height);
+        return current;
+    }
+
+    protected abstract StreamLoader buildStreamOpener(T model, int width, int height);
+
+    @Override
+    public void clear() {
+        if (current != null) {
+            current.cancel();
+            current = null;
+        }
+    }
+}
