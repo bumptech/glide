239979db20a8f6ef11ab36f16ce72afb79e89bed
Sam Judd <sam@bu.mp>
2013-09-13 09:40:21 -0700

    Refactor and add tests for ImagePresenter

--- library/tests/src/com/bumptech/glide/ImagePresenterTest.java
+++ library/tests/src/com/bumptech/glide/ImagePresenterTest.java
@@ -1,18 +1,25 @@
 package com.bumptech.glide;
 
 import android.R;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.test.AndroidTestCase;
-import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.view.animation.Animation;
 import com.bumptech.glide.loader.image.ImageLoader;
 import com.bumptech.glide.loader.image.ImageManagerLoader;
 import com.bumptech.glide.loader.model.ModelLoader;
 import com.bumptech.glide.loader.stream.StreamLoader;
 import com.bumptech.glide.presenter.ImagePresenter;
+import com.bumptech.glide.presenter.ImageReadyCallback;
+import com.bumptech.glide.presenter.target.Target;
 import com.bumptech.glide.resize.load.Transformation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,13 +30,12 @@ import com.bumptech.glide.resize.load.Transformation;
  */
 public class ImagePresenterTest extends AndroidTestCase {
 
-    private ImageView imageView;
+    private MockTarget target;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        imageView = new ImageView(getContext());
-        imageView.setLayoutParams(new ViewGroup.LayoutParams(100, 100));
+        target = new MockTarget();
     }
 
     public void testCanSetErrorDrawable() {
@@ -65,44 +71,363 @@ public class ImagePresenterTest extends AndroidTestCase {
         assertNotNull(exception);
     }
 
+    public void testMustProvideATarget() {
+        Exception exception = null;
+        try {
+            new ImagePresenter.Builder<Object>()
+                    .setModelLoader(new MockObjectLoader())
+                    .setImageLoader(new MockImageLoader())
+                    .build();
+        } catch (Exception e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    public void testMustProvideAModelLoader() {
+        Exception exception = null;
+        try {
+            new ImagePresenter.Builder<Object>()
+                    .setTarget(target, getContext())
+                    .setImageLoader(new MockImageLoader())
+                    .build();
+        } catch (Exception e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    public void testMustProvideAnImageLoader() {
+        Exception exception = null;
+        try {
+            new ImagePresenter.Builder<Object>()
+                    .setTarget(target, getContext())
+                    .setModelLoader(new MockObjectLoader())
+                    .build();
+        } catch (Exception e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    public void testPlaceholderIsSetWithNullModel() {
+        Drawable placeholder = new ColorDrawable(Color.RED);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new ImageManagerLoader(getContext()))
+                .setPlaceholderDrawable(placeholder)
+                .build();
+        imagePresenter.setModel(null);
+        assertEquals(placeholder, target.getPlaceholder());
+    }
+
+    public void testPlaceholderIsSetWithNullId() {
+        Drawable placeholder = new ColorDrawable(Color.RED);
+        final Bitmap result = Bitmap.createBitmap(1, 1, Bitmap.Config.ALPHA_8);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader() {
+                    @Override
+                    public String getId(Object model) {
+                        return null;
+                    }
+                })
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cb.onImageReady(result);
+                    }
+                }))
+                .setPlaceholderDrawable(placeholder)
+                .build();
+
+        imagePresenter.setModel(new Object());
+        assertEquals(placeholder, target.getPlaceholder());
+    }
+
+    public void testPlaceholderIsSetWithNullStreamLoader() {
+        Drawable placeholder = new ColorDrawable(Color.RED);
+        final Bitmap result = Bitmap.createBitmap(1, 1, Bitmap.Config.ALPHA_8);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader() {
+                    @Override
+                    public StreamLoader getStreamLoader(Object model, int width, int height) {
+                        return null;
+                    }
+                })
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cb.onImageReady(result);
+                    }
+                }))
+                .setPlaceholderDrawable(placeholder)
+                .build();
+
+        imagePresenter.setModel(new Object());
+        assertEquals(placeholder, target.getPlaceholder());
+    }
+
+    public void testPlaceholderIsSetDuringAsynchronousLoad() {
+        Drawable placeholder = new ColorDrawable(Color.RED);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader())
+                .setPlaceholderDrawable(placeholder)
+                .build();
+
+        imagePresenter.setModel(new Object());
+        assertEquals(placeholder, target.getPlaceholder());
+    }
+
+    public void testPlaceholderIsNotSetDuringSynchronousLoad() {
+        Drawable placeholder = new ColorDrawable(Color.RED);
+        final Bitmap result = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_4444);
+
+        final AtomicBoolean wasPlaceholderEverSet = new AtomicBoolean(false);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(new MockTarget() {
+                    @Override
+                    public void setPlaceholder(Drawable placeholder) {
+                        wasPlaceholderEverSet.set(true);
+                        super.setPlaceholder(placeholder);
+                    }
+                }, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cb.onImageReady(result);
+                    }
+                }))
+                .setPlaceholderDrawable(placeholder)
+                .build();
+
+        imagePresenter.setModel(new Object());
+        assertFalse(wasPlaceholderEverSet.get());
+    }
+
     public void testErrorPlaceholderIsSetOnException() {
         Drawable errorDrawable = new ColorDrawable(Color.RED);
         ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
-                .setImageView(imageView)
-                .setModelLoader(new ModelLoader<Object>() {
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
                     @Override
-                    public StreamLoader getStreamLoader(Object model, int width, int height) {
-                        return new StreamLoader() {
-                            @Override
-                            public void loadStream(StreamReadyCallback cb) {
-                            }
-
-                            @Override
-                            public void cancel() {
-                            }
-                        };
-                    }
-
-                    @Override
-                    public String getId(Object model) {
-                        return model.toString();
-                    }
-                })
-                .setImageLoader(new ImageLoader() {
-                    @Override
-                    public Object fetchImage(String id, StreamLoader streamLoader, Transformation transformation, int width, int height, ImageReadyCallback cb) {
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
                         cb.onException(new Exception("Test"));
-                        return null;
                     }
-
-                    @Override
-                    public void clear() { }
-                })
+                }))
                 .setErrorDrawable(errorDrawable)
                 .build();
 
-        assertNull(imageView.getDrawable());
         imagePresenter.setModel(new Object());
-        assertEquals(errorDrawable, imageView.getDrawable());
+        assertEquals(errorDrawable, target.getPlaceholder());
+    }
+
+    public void testBitmapIsSetWhenLoaded() {
+        final Bitmap result = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cb.onImageReady(result);
+                    }
+                }))
+                .build();
+        imagePresenter.setModel(new Object());
+        assertEquals(result, target.getBitmap());
+    }
+
+    public void testOldLoadsAreIgnoredIfNewFinishFirst() {
+        final Bitmap first = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+        final Bitmap second = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_4444);
+
+        final List<ImageLoader.ImageReadyCallback> cbs = new ArrayList<ImageLoader.ImageReadyCallback>();
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cbs.add(cb);
+                    }
+                }))
+                .build();
+
+        imagePresenter.setModel(new Object());
+        imagePresenter.setModel(new Object());
+
+        cbs.get(1).onImageReady(second);
+        cbs.get(0).onImageReady(first);
+        assertEquals(second, target.getBitmap());
+    }
+
+    public void testExceptionHandlerIsCalledOnException() {
+        final Exception expected = new Exception("Test");
+        final Object actualModel = new Object();
+
+        final AtomicBoolean wasOnExceptionCalled = new AtomicBoolean(false);
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cb.onException(expected);
+                    }
+                }))
+                .setExceptionHandler(new ImagePresenter.ExceptionHandler<Object>() {
+                    @Override
+                    public void onException(Exception e, Object model, boolean isCurrent) {
+                        assertEquals(expected, e);
+                        assertEquals(actualModel, model);
+                        wasOnExceptionCalled.set(true);
+                    }
+                })
+                .build();
+
+        imagePresenter.setModel(actualModel);
+
+        assertTrue(wasOnExceptionCalled.get());
+    }
+
+    public void testSettingSameModelTwiceInARowIsNoOp() {
+        final Object model = new Object();
+        final AtomicInteger timesMockImageLoaderCalled = new AtomicInteger();
+
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        timesMockImageLoaderCalled.incrementAndGet();
+                    }
+                }))
+                .build();
+
+        imagePresenter.setModel(model);
+        imagePresenter.setModel(model);
+
+        assertEquals(1, timesMockImageLoaderCalled.get());
+    }
+
+    public void testImageSetCallbackIsCalledWhenImageIsSet() {
+        final AtomicBoolean wasImageSetCallbackCalled = new AtomicBoolean(false);
+
+        ImagePresenter<Object> imagePresenter = new ImagePresenter.Builder<Object>()
+                .setTarget(target, getContext())
+                .setModelLoader(new MockObjectLoader())
+                .setImageLoader(new MockImageLoader().onCallback(new MockImageLoader.CallbackAction() {
+                    @Override
+                    public void onCallbackReceived(ImageLoader.ImageReadyCallback cb) {
+                        cb.onImageReady(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_4444));
+                    }
+                }))
+                .setImageReadyCallback(new ImageReadyCallback() {
+                    @Override
+                    public void onImageReady(Target target, boolean fromCache) {
+                        wasImageSetCallbackCalled.set(true);
+                    }
+                })
+                .build();
+
+        imagePresenter.setModel(new Object());
+        assertTrue(wasImageSetCallbackCalled.get());
+    }
+
+    private static class MockTarget implements Target {
+
+        private Bitmap bitmap = null;
+        private Drawable placeholder = null;
+
+        public Bitmap getBitmap() {
+            return bitmap;
+        }
+
+        public Drawable getPlaceholder() {
+            return placeholder;
+        }
+
+        @Override
+        public void onImageReady(Bitmap bitmap) {
+            placeholder = null;
+            this.bitmap = bitmap;
+        }
+
+        @Override
+        public void setPlaceholder(Drawable placeholder) {
+            bitmap = null;
+            this.placeholder = placeholder;
+        }
+
+        @Override
+        public void getSize(SizeReadyCallback cb) {
+            cb.onSizeReady(1, 1);
+        }
+
+        @Override
+        public void startAnimation(Animation animation) {
+        }
+
+        @Override
+        public void setImagePresenter(ImagePresenter imagePresenter) {
+        }
+
+        @Override
+        public ImagePresenter getImagePresenter() {
+            return null;
+        }
+    }
+
+    private static class MockImageLoader implements ImageLoader {
+        private CallbackAction action;
+
+        public interface CallbackAction {
+            public void onCallbackReceived(ImageReadyCallback cb);
+        }
+
+        public MockImageLoader onCallback(CallbackAction action) {
+            this.action = action;
+            return this;
+        }
+
+        @Override
+        public Object fetchImage(String id, StreamLoader streamLoader, Transformation transformation, int width, int height, ImageReadyCallback cb) {
+            if (action != null) {
+                action.onCallbackReceived(cb);
+            }
+            return null;
+        }
+
+        @Override
+        public void clear() { }
+    }
+
+    private static class MockObjectLoader implements ModelLoader<Object> {
+
+        @Override
+        public StreamLoader getStreamLoader(Object model, int width, int height) {
+            return new StreamLoader() {
+                @Override
+                public void loadStream(StreamReadyCallback cb) {
+                }
+
+                @Override
+                public void cancel() {
+                }
+            };
+        }
+
+        @Override
+       public String getId(Object model) {
+            return model.toString();
+        }
     }
 }
