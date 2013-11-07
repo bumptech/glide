855a789288e0f1947d4e2558cb1719a1dddb31d2
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2013-11-07 15:43:00 +0000

    Fixing import order

--- library/src/com/bumptech/glide/presenter/target/ImageViewTarget.java
+++ library/src/com/bumptech/glide/presenter/target/ImageViewTarget.java
@@ -1,7 +1,5 @@
 package com.bumptech.glide.presenter.target;
 
-import static android.view.ViewGroup.LayoutParams;
-
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
@@ -9,6 +7,8 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import com.bumptech.glide.presenter.ImagePresenter;
+
+import static android.view.ViewGroup.LayoutParams;
 
 /**
  * A target wrapping an ImageView. Obtains the runtime dimensions of the ImageView.
