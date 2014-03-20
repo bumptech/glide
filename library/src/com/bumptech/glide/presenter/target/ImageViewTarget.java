635d04fddf1b28a798806263f4f286df02bf79d2
Sam Judd <judds@google.com>
2014-03-20 08:03:02 -0700

    Add more robust view size detection.
    Includes at least functional support for 
    wrap_content.

--- library/src/com/bumptech/glide/presenter/target/ImageViewTarget.java
+++ library/src/com/bumptech/glide/presenter/target/ImageViewTarget.java
@@ -1,12 +1,18 @@
 package com.bumptech.glide.presenter.target;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.os.Handler;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import com.bumptech.glide.presenter.ImagePresenter;
+
+import java.lang.ref.WeakReference;
 
 import static android.view.ViewGroup.LayoutParams;
 
@@ -14,6 +20,7 @@ import static android.view.ViewGroup.LayoutParams;
  * A target wrapping an ImageView. Obtains the runtime dimensions of the ImageView.
  */
 public class ImageViewTarget implements Target {
+    private static final String TAG = "ImageViewTarget";
     private final ImageView imageView;
     private final SizeDeterminer sizeDeterminer;
     private ImagePresenter imagePresenter;
@@ -72,56 +79,101 @@ public class ImageViewTarget implements Target {
     }
 
     private static class SizeDeterminer {
-        private static final String PENDING_SIZE_CHANGE_TOKEN = "pending_load";
-        private static final int PENDING_SIZE_CHANGE_DELAY = 100; //60 fps = 1000/60 = 16.67 ms
-
         private final View view;
-        private int[] dimens = new int[2];
-        private boolean valid = false;
-        private SizeReadyCallback cb = null;
-        private Handler handler = new Handler();
-        private final Runnable getDimens = new Runnable() {
-            @Override
-            public void run() {
-                if (cb == null) return;
+        private SizeReadyCallback cb;
+        private SizeDeterminerLayoutListener layoutListener;
 
-                LayoutParams layoutParams = view.getLayoutParams();
-                if (layoutParams.width > 0 && layoutParams.height > 0) {
-                    cb.onSizeReady(layoutParams.width, layoutParams.height);
-                } else if (view.getWidth() > 0 && view.getHeight() > 0) {
-                    valid = true;
-                    dimens[0] = view.getWidth();
-                    dimens[1] = view.getHeight();
-                    cb.onSizeReady(dimens[0], dimens[1]);
-                }
-                cb = null;
+        private void checkCurrentDimens() {
+            if (cb == null) return;
+
+            boolean calledCallback = true;
+            LayoutParams layoutParams = view.getLayoutParams();
+            if (isViewSizeValid()) {
+                cb.onSizeReady(view.getWidth(), view.getHeight());
+            } else if (isLayoutParamsSizeValid()) {
+                cb.onSizeReady(layoutParams.width, layoutParams.height);
+            } else {
+                calledCallback = false;
             }
-        };
+
+            if (calledCallback) {
+                cb = null;
+                // Keep a reference to the layout listener and remove it here
+                // rather than having the observer remove itself because the observer
+                // we add the listener to will be almost immediately merged into
+                // another observer and will therefore never be alive. If we instead
+                // keep a reference to the listener and remove it here, we get the
+                // current view tree observer and should succeed.
+                ViewTreeObserver observer = view.getViewTreeObserver();
+                if (observer.isAlive()) {
+                    observer.removeGlobalOnLayoutListener(layoutListener);
+                }
+            }
+        }
 
         public SizeDeterminer(View view) {
             this.view = view;
         }
 
         public void getSize(SizeReadyCallback cb) {
-            handler.removeCallbacksAndMessages(PENDING_SIZE_CHANGE_TOKEN);
             this.cb = null;
             LayoutParams layoutParams = view.getLayoutParams();
-            //non null layout params and either width and height have been set, or set to wrap content so they
-            //will not be set until we set some content
-            if (isHandled(layoutParams)) {
+            if (isViewSizeValid()) {
+                cb.onSizeReady(view.getWidth(), view.getHeight());
+            } else if (isLayoutParamsSizeValid()) {
                 cb.onSizeReady(layoutParams.width, layoutParams.height);
-            } else if (valid) {
-                cb.onSizeReady(dimens[0], dimens[1]);
+            } else if (isUsingWrapContent()) {
+                WindowManager windowManager =
+                        (WindowManager) view.getContext().getSystemService(Context.WINDOW_SERVICE);
+                Display display = windowManager.getDefaultDisplay();
+                final int width = display.getWidth();
+                final int height = display.getHeight();
+                if (Log.isLoggable(TAG, Log.WARN)) {
+                    Log.w(TAG, "Trying to load image into ImageView using WRAP_CONTENT, defaulting to screen" +
+                            " dimensions: [" + width + "x" + height + "]. Give the view an actual width and height " +
+                            " for better performance.");
+                }
+                cb.onSizeReady(display.getWidth(), display.getHeight());
             } else {
                 this.cb = cb;
-                handler.postDelayed(getDimens, PENDING_SIZE_CHANGE_DELAY);
+                final ViewTreeObserver observer = view.getViewTreeObserver();
+                layoutListener = new SizeDeterminerLayoutListener(this);
+                observer.addOnGlobalLayoutListener(layoutListener);
             }
         }
 
-        private boolean isHandled(LayoutParams layoutParams) {
-            return layoutParams != null && ((layoutParams.width > 0 && layoutParams.height > 0) ||
-                    (layoutParams.width == LayoutParams.WRAP_CONTENT || layoutParams.height == LayoutParams.WRAP_CONTENT));
+        private boolean isViewSizeValid() {
+            return view.getWidth() > 0 && view.getHeight() > 0;
         }
 
+        private boolean isUsingWrapContent() {
+            final LayoutParams layoutParams = view.getLayoutParams();
+            return layoutParams != null && (layoutParams.width == LayoutParams.WRAP_CONTENT
+                    || layoutParams.height == LayoutParams.WRAP_CONTENT);
+        }
+
+        private boolean isLayoutParamsSizeValid() {
+            final LayoutParams layoutParams = view.getLayoutParams();
+            return layoutParams != null && (layoutParams.width > 0 && layoutParams.height > 0);
+        }
+
+        private static class SizeDeterminerLayoutListener implements ViewTreeObserver.OnGlobalLayoutListener {
+            private final WeakReference<SizeDeterminer> sizeDeterminerRef;
+
+            public SizeDeterminerLayoutListener(SizeDeterminer sizeDeterminer) {
+                sizeDeterminerRef = new WeakReference<SizeDeterminer>(sizeDeterminer);
+            }
+
+            @Override
+            public void onGlobalLayout() {
+                if (Log.isLoggable(TAG, Log.VERBOSE)) {
+                    Log.v(TAG, "OnGlobalLayoutListener called listener=" + this);
+                }
+                SizeDeterminer sizeDeterminer = sizeDeterminerRef.get();
+                if (sizeDeterminer != null) {
+                    sizeDeterminer.checkCurrentDimens();
+                }
+            }
+        }
     }
 }
