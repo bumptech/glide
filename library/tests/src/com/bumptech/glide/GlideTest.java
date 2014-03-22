1973ac99527d3648a7b0bbeb6a273dbcc3bc60ba
Sam Judd <judds@google.com>
2014-03-21 19:20:00 -0700

    Don't test removed functionality.

--- library/tests/src/com/bumptech/glide/GlideTest.java
+++ library/tests/src/com/bumptech/glide/GlideTest.java
@@ -1,21 +1,15 @@
 package com.bumptech.glide;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.test.ActivityTestCase;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import com.bumptech.glide.loader.bitmap.model.stream.StreamModelLoader;
-import com.bumptech.glide.loader.bitmap.resource.ResourceFetcher;
 import com.bumptech.glide.presenter.ImagePresenter;
 import com.bumptech.glide.presenter.target.ImageViewTarget;
-import com.bumptech.glide.resize.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.presenter.target.Target;
-import com.bumptech.glide.tests.R;
 
 import java.io.File;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -83,30 +77,6 @@ public class GlideTest extends ActivityTestCase {
         checkImagePresenter(target, integer);
     }
 
-    public void testGlideDoesNotReplaceIdenticalPresenters() {
-        Target target = Glide.with(getContext())
-                .load("fake")
-                .asIs()
-                .centerCrop()
-                .animate(android.R.anim.fade_in)
-                .placeholder(com.bumptech.glide.tests.R.raw.ic_launcher)
-                .error(com.bumptech.glide.tests.R.raw.ic_launcher)
-                .into(imageViewTarget);
-        ImagePresenter first = target.getImagePresenter();
-
-        Target target2 = Glide.with(getContext())
-                .load("fake2")
-                .asIs()
-                .centerCrop()
-                .animate(android.R.anim.fade_in)
-                .placeholder(com.bumptech.glide.tests.R.raw.ic_launcher)
-                .error(com.bumptech.glide.tests.R.raw.ic_launcher)
-                .into(imageViewTarget);
-        ImagePresenter second = target2.getImagePresenter();
-
-        assertSame(first, second);
-    }
-
     public void testCanHandleWrapContent() {
         imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -121,150 +91,6 @@ public class GlideTest extends ActivityTestCase {
         Target target = Glide.with(getContext()).load("fake").into(imageViewTarget);
 
         assertNotNull(target.getImagePresenter());
-    }
-
-    public void testDifferentModelTypesReplacesPresenters() {
-        assertDifferentPresenters(
-                Glide.with(getContext()).load(4),
-                Glide.with(getContext()).load("fake")
-        );
-    }
-
-    public void testDifferentModelLoadersReplacesPresenter() {
-        StreamModelLoader<Object> first = new StreamModelLoader<Object>() {
-
-            @Override
-            public ResourceFetcher<InputStream> getResourceFetcher(Object model, int width, int height) {
-                return new ResourceFetcher<InputStream>() {
-                    @Override
-                    public InputStream loadResource() throws Exception {
-                        return null;
-                    }
-
-                    @Override
-                    public String getId() {
-                        return null;
-                    }
-
-                    @Override
-                    public void cancel() {
-                    }
-                };
-            }
-
-            @Override
-            public String getId(Object model) {
-                return model.toString();
-            }
-
-        };
-
-        StreamModelLoader<Object> second = new StreamModelLoader<Object>() {
-            @Override
-            public ResourceFetcher<InputStream> getResourceFetcher(Object model, int width, int height) {
-                return new ResourceFetcher<InputStream>() {
-                    @Override
-                    public InputStream loadResource() throws Exception {
-                        return null;
-                    }
-
-                    @Override
-                    public String getId() {
-                        return null;
-                    }
-
-                    @Override
-                    public void cancel() {
-                    }
-                };
-            }
-
-            @Override
-            public String getId(Object model) {
-                return model.toString();
-            }
-        };
-
-        final Object object = new Object();
-        assertDifferentPresenters(
-                Glide.with(getContext()).using(first).load(object),
-                Glide.with(getContext()).using(second).load(object)
-        );
-    }
-
-    public void testDifferentDownsamplersReplacesPresenter() {
-        assertDifferentPresenters(
-                Glide.with(getContext()).load("a").approximate(),
-                Glide.with(getContext()).load("a").asIs()
-        );
-    }
-
-    public void testDifferentTransformationsReplacesPresenter() {
-        final File file = new File("fake");
-        assertDifferentPresenters(
-                Glide.with(getContext()).load(file).centerCrop().fitCenter(),
-                Glide.with(getContext()).load(file).centerCrop()
-        );
-    }
-
-    public void testDifferentPlaceholdersReplacesPresenter() {
-        final File file = new File("fake");
-        assertDifferentPresenters(
-                Glide.with(getContext()).load(file).placeholder(com.bumptech.glide.tests.R.raw.ic_launcher),
-                Glide.with(getContext()).load(file)
-
-        );
-    }
-
-    public void testDifferentAnimationsReplacesPresenter() {
-        final File file = new File("fake");
-        assertDifferentPresenters(
-                Glide.with(getContext()).load(file).animate(android.R.anim.fade_in),
-                Glide.with(getContext()).load(file).animate(android.R.anim.fade_out)
-        );
-    }
-
-    public void testDifferentErrorIdsReplacesPresenter() {
-        assertDifferentPresenters(
-                Glide.with(getContext()).load("b").error(R.raw.ic_launcher),
-                Glide.with(getContext()).load("b").error(android.R.drawable.btn_star)
-        );
-    }
-
-    public void testObtainAndOfferToBitmapPool() {
-        Bitmap small = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Bitmap large = Bitmap.createBitmap(512, 512, Bitmap.Config.RGB_565);
-        BitmapPool bitmapPool = Glide.get().getImageManager(getInstrumentation().getContext()).getBitmapPool();
-        bitmapPool.put(small);
-        bitmapPool.put(large);
-
-        assertEquals(small, bitmapPool.get(small.getWidth(), small.getHeight(), small.getConfig()));
-        assertEquals(large, bitmapPool.get(large.getWidth(), large.getHeight(), large.getConfig()));
-    }
-
-    public void testDifferentRequestListenersReplacesPresenter() {
-        assertDifferentPresenters(
-                Glide.with(getContext()).load("a").listener(new Glide.RequestListener<String>() {
-
-                    @Override
-                    public void onException(Exception e, String model, Target target) {
-
-                    }
-
-                    @Override
-                    public void onImageReady(String model, Target target) {
-                    }
-                }),
-                Glide.with(getContext()).load("a").listener(new Glide.RequestListener<String>() {
-                    @Override
-                    public void onException(Exception e, String model, Target target) {
-                    }
-
-                    @Override
-                    public void onImageReady(String model, Target target) {
-                    }
-                })
-        );
     }
 
 //    public void testMemoryLeak() throws InterruptedException {
@@ -334,13 +160,4 @@ public class GlideTest extends ActivityTestCase {
 //        handler.post(doLoad);
 //        handlerThread.join();
 //    }
-
-    private void assertDifferentPresenters(Glide.Request a, Glide.Request b) {
-        ImagePresenter first = a.into(imageViewTarget).getImagePresenter();
-        ImagePresenter second = a.into(imageViewTarget).getImagePresenter();
-        ImagePresenter third = b.into(imageViewTarget).getImagePresenter();
-
-        assertSame(first, second);
-        assertNotSame(first, third);
-    }
 }
