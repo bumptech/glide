f1d30908374cbc8a570d2cb1ca22ba6dfe3e0d4a
Sam Judd <judds@google.com>
2014-06-22 13:49:46 -0700

    Working auto import into intellij using pom files

--- library/install_dependencies/install-android-deps.sh
+++ library/install_dependencies/install-android-deps.sh
@@ -6,13 +6,24 @@
 # Using the android sdk tool, under Android 4.4.2 (API 19), install:
 #   SDK Platform
 #   Glass Development Kit Preview
-#      
+# Also using the android sdk tool, under Android 4.4.0 (API 14), install:
+#   SDK Platform
 
 git clone https://github.com/mosabua/maven-android-sdk-deployer.git
 cd maven-android-sdk-deployer 
-mvn clean install -N && cd platforms && mvn clean install -N && cd android-19 && mvn clean install || { 
-  echo 'Failed to install 4.4 SDK, install relevant packages in android SDK first'; 
+mvn clean install -N && cd platforms && mvn clean install -N && cd android-19 && mvn clean install || {
+  echo 'Failed to install 4.4 SDK, install relevant packages in android SDK first';
   exit 1; 
 }
 cd ../..
-mvn clean install -N && cd extras && mvn clean install -N && cd compatibility-v4 && mvn clean install || { echo 'Failed to install android-support-v4, install support library in android SDK first' ; exit 1; }
+mvn clean install -N && cd platforms && mvn clean install -N && cd android-14 && mvn clean install || {
+  echo 'Failed to install 4.0 SDK, install relevant packages in android SDK first';
+  exit 1;
+}
+cd ../..
+mvn clean install -N && cd extras && mvn clean install -N && cd compatibility-v4 && mvn clean install || {
+  echo 'Failed to install android-support-v4, install support library in android SDK first';
+  exit 1;
+}
+cd ../../..
+rm -rf maven-android-sdk-deployer
