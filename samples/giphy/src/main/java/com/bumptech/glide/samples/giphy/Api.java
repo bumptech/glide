ed2140817d86cc3ad9067d451c5fa0a6e362e04a
Sam Judd <judds@google.com>
2014-07-11 07:52:10 -0700

    Add a gif based sample project.

---
+++ samples/giphy/src/main/java/com/bumptech/glide/samples/giphy/Api.java
@@ -0,0 +1,192 @@
+package com.bumptech.glide.samples.giphy;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Based on https://github.com/Giphy/GiphyAPI.
+ */
+public final class Api {
+    private static Api api = null;
+    private static final String BETA_KEY = "dc6zaTOxFJmzC";
+    private static final String BASE_URL = "http://api.giphy.com";
+    private static final String SEARCH_PATH = "/v1/gifs/search";
+    private static final String TRENDING_PATH = "/v1/gifs/trending";
+    private final Handler bgHandler;
+    private final Handler mainHandler;
+    private final HashSet<Monitor> monitors = new HashSet<Monitor>();
+
+    private static String signUrl(String url) {
+        return url + "&api_key=" + BETA_KEY;
+    }
+
+    private static String getSearchUrl(String query, int limit, int offset) {
+        return signUrl(BASE_URL + SEARCH_PATH + "?q=" + query + "&limit=" + limit + "&offset=" + offset);
+    }
+
+    private static String getTrendingUrl(int limit, int offset) {
+        return signUrl(BASE_URL + TRENDING_PATH + "?limit=" + limit + "&offset=" + offset);
+    }
+
+
+    public interface Monitor {
+        public void onSearchComplete(SearchResult result);
+    }
+
+    public static Api get() {
+        if (api == null) {
+            synchronized (Api.class) {
+                if (api == null) {
+                    api = new Api();
+                }
+            }
+        }
+        return api;
+    }
+
+    private Api() {
+        HandlerThread bgThread = new HandlerThread("api_thread");
+        bgThread.start();
+        bgHandler = new Handler(bgThread.getLooper());
+        mainHandler = new Handler(Looper.getMainLooper());
+        // Do nothing.
+    }
+
+    public void addMonitor(Monitor monitor) {
+        monitors.add(monitor);
+    }
+
+    public void removeMonitor(Monitor monitor) {
+        monitors.remove(monitor);
+    }
+
+    public void search(String searchTerm) {
+        String searchUrl = getSearchUrl(searchTerm, 100, 0);
+        query(searchUrl);
+    }
+
+    public void getTrending() {
+        String trendingUrl = getTrendingUrl(100, 0);
+        query(trendingUrl);
+    }
+
+    private void query(final String apiUrl) {
+        bgHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                URL url;
+                try {
+                    url = new URL(apiUrl);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                HttpURLConnection urlConnection = null;
+                InputStream is = null;
+                SearchResult result = new SearchResult();
+                try {
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    is = urlConnection.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(is);
+                    result = new Gson().fromJson(reader, SearchResult.class);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            // Do nothing.
+                        }
+                    }
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+
+                final SearchResult finalResult = result;
+                mainHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (Monitor monitor : monitors) {
+                            monitor.onSearchComplete(finalResult);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    public static class SearchResult {
+        public GifResult[] data;
+
+        @Override
+        public String toString() {
+            return "SearchResult{"
+                    + "data=" + Arrays.toString(data)
+                    + '}';
+        }
+    }
+
+    public static class GifResult {
+        public String id;
+        // Page url not gif url
+        public String url;
+        public GifUrlSet images;
+
+        @Override
+        public String toString() {
+            return "GifResult{"
+                    + "id='" + id + '\''
+                    + ", url='" + url + '\''
+                    + ", images=" + images
+                    + '}';
+        }
+    }
+
+    public static class GifUrlSet {
+        public GifImage original;
+        public GifImage fixed_width_downsampled;
+        public GifImage fixed_height_downsampled;
+
+        @Override
+        public String toString() {
+            return "GifUrlSet{"
+                    + "original=" + original
+                    + ", fixed_width_downsampled=" + fixed_width_downsampled
+                    + ", fixed_height_downsampled=" + fixed_height_downsampled
+                    + '}';
+        }
+    }
+
+    public static class GifImage {
+        public String url;
+        public int width;
+        public int height;
+        public int frames;
+        public int size;
+
+        @Override
+        public String toString() {
+            return "GifImage{"
+                    + "url='" + url + '\''
+                    + ", width=" + width
+                    + ", height=" + height
+                    + ", frames=" + frames
+                    + ", size=" + size
+                    + '}';
+        }
+    }
+}
---
+++ samples/giphy/src/main/java/com/bumptech/glide/samples/giphy/MainActivity.java
@@ -0,0 +1,98 @@
+package com.bumptech.glide.samples.giphy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.bumptech.glide.Glide;
+
+
+public class MainActivity extends Activity implements Api.Monitor {
+    private static final String TAG = "GiphyActivity";
+
+    private GifAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Api.get().getTrending();
+
+        ImageView giphyLogoView = (ImageView) findViewById(R.id.giphy_logo_view);
+        Glide.with(this)
+                .load(R.raw.large_giphy_logo)
+                .fitCenter()
+                .into(giphyLogoView);
+
+        ListView gifList = (ListView) findViewById(R.id.gif_list);
+        adapter = new GifAdapter();
+        gifList.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Api.get().addMonitor(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Api.get().removeMonitor(this);
+    }
+
+    @Override
+    public void onSearchComplete(Api.SearchResult result) {
+        adapter.setResults(result.data);
+    }
+
+    private class GifAdapter extends BaseAdapter {
+
+        private Api.GifResult[] results = new Api.GifResult[0];
+
+        public void setResults(Api.GifResult[] results) {
+            this.results = results;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return results.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.gif_list_item, parent, false);
+            }
+
+            Api.GifResult result = results[position];
+            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                Log.d(TAG, "load result: " + result);
+            }
+            ImageView gifView = (ImageView) convertView.findViewById(R.id.gif_view);
+
+            Glide.with(MainActivity.this)
+                    .load(result.images.original.url)
+                    .fitCenter()
+                    .into(gifView);
+
+            return convertView;
+        }
+    }
+}
