1cd0b7c7f697e82df47febe9713ed1c8f981d53d
Sam Judd <judds@google.com>
2014-08-31 15:45:01 -0700

    Move env variables to travis settings.

--- scripts/travis-sonatype-publish.sh
+++ scripts/travis-sonatype-publish.sh
@@ -3,12 +3,7 @@
 # Based closely on material from
 # http://benlimmer.com/2014/01/04/automatically-publish-to-sonatype-with-gradle-and-travis-ci.
 
-if [ "$TRAVIS_REPO_SLUG" == "bumptech/glide" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "3.0a" ]; then
-  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
-      echo 'Travis can only publish snapshots.'
-      return 0
-  fi
-
+if [ "$SONATYPE_PUBLISH" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$SONATYPE_PUBLISH_BRANCH" ]; then
   echo -e "Starting publish to Sonatype...\n"
 
   ./gradlew uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}"
