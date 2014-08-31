2d2fd74ee83002c19541f772dfb3e5ff5aa967c1
Robert Papp <papp.robert.s@gmail.com>
2014-08-31 17:06:59 +0200

    Extract SimpleResource as a reusable component (clients may use it).

---
+++ library/src/main/java/com/bumptech/glide/load/resource/SimpleResource.java
@@ -0,0 +1,33 @@
+package com.bumptech.glide.load.resource;
+
+import com.bumptech.glide.load.engine.Resource;
+
+/**
+ * Simple wrapper for an arbitrary object which helps to satisfy some of the glide engine's contracts.
+ * <b>Suggested usages only include resource object which don't have size and cannot be recycled/closed.</b>
+ *
+ * @param <T> type of the wrapped resource
+ */
+// TODO: there isn't much point in caching these...
+public class SimpleResource<T> implements Resource<T> {
+    protected final T data;
+
+    public SimpleResource(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public final T get() {
+        return data;
+    }
+
+    @Override
+    public final int getSize() {
+        return 1;
+    }
+
+    @Override
+    public void recycle() {
+        // no op
+    }
+}
