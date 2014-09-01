b0fb75dd3edca27e11f2101c5f57e3d7abaf137e
Robert Papp <papp.robert.s@gmail.com>
2014-09-01 15:20:57 +0200

    Review: Remove unnecessary check

--- samples/svg/src/main/java/com/bumptech/svgsample/app/SvgDecoder.java
+++ samples/svg/src/main/java/com/bumptech/svgsample/app/SvgDecoder.java
@@ -1,7 +1,5 @@
 package com.bumptech.svgsample.app;
 
-import android.annotation.SuppressLint;
-import android.os.Build;
 import com.bumptech.glide.load.ResourceDecoder;
 import com.bumptech.glide.load.engine.Resource;
 import com.caverock.androidsvg.SVG;
@@ -14,15 +12,11 @@ import java.io.InputStream;
  * Decodes an SVG internal representation from an {@link InputStream}.
  */
 public class SvgDecoder implements ResourceDecoder<InputStream, SVG> {
-    @SuppressLint("NewApi")
     public Resource<SVG> decode(InputStream source, int width, int height) throws IOException {
         try {
             SVG svg = SVG.getFromInputStream(source);
             return new SvgResource(svg);
         } catch (SVGParseException ex) {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
-                throw new IOException(ex.toString());
-            }
             throw new IOException("Cannot load SVG from stream", ex);
         }
     }
