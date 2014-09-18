634650b0ad234e857e7afcc25818e8d0306b2910
Sam Judd <judds@google.com>
2014-09-17 19:40:21 -0700

    Update FutureTarget documentation.
    Fixes #142.

--- library/src/main/java/com/bumptech/glide/request/FutureTarget.java
+++ library/src/main/java/com/bumptech/glide/request/FutureTarget.java
@@ -6,13 +6,13 @@ import java.util.concurrent.Future;
 
 /**
  * An interface for an object that is both a {@link com.bumptech.glide.request.target.Target} and a
- * {@link java.util.concurrent.Future}. Use as follows:
+ * {@link java.util.concurrent.Future}. For example:
  * <pre>
  * {@code
  * FutureTarget<Bitmap> futureTarget = Glide.with(fragment)
  *                                       .load("http://goo.gl/1asf12")
  *                                       .asBitmap()
- *                                       .intoFuture(250, 250);
+ *                                       .into(250, 250);
  * Bitmap myBitmap = futureTarget.get();
  * ... // do things with bitmap and then release when finished:
  * Glide.clear(futureTarget);
