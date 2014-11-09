99ae810529d481e215a6f9bcd2070e5c951c76cf
Pavlos-Petros Tournaris <p.tournaris@gmail.com>
2014-11-09 12:35:16 +0200

    Indentation fixes and other improvements.

--- library/src/main/java/com/bumptech/glide/request/target/RemoteViewsTarget.java
+++ library/src/main/java/com/bumptech/glide/request/target/RemoteViewsTarget.java
@@ -12,33 +12,32 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 
 public abstract class RemoteViewsTarget<Z> extends BaseTarget<Z> {
 
-    private int mViewId;
-    private int mWidth, mHeight;
+    private final int mViewId;
+    private final int mWidth, mHeight;
     private int[] mWidgetIds;
     private final RemoteViews mRemoteViews;
-    private AppWidgetManager mAppWidgetManager;
-    private Context mContext;
+    private final AppWidgetManager mAppWidgetManager;
+    private final Context mContext;
     private ComponentName mComponentName = null;
 
     /**
-     *
      * Constructor using an int array of widgetIds,
      * to get a handle on the Widget,
      * in order to update it.
      *
      * @param remoteViews RemoteViews object which contains the ImageView that will load the bitmap.
-     * @param context Context to use in the AppWidgetManager initialization.
-     * @param viewId The id of the ImageView view that will load the image.
-     * @param width Desired width of the bitmap that will be loaded.(Need to be manually set because of RemoteViews limitations.)
-     * @param height Desired height of the bitmap that will be loaded. (Need to be manually set because of RemoteViews limitations.)
-     * @param widgetIds The int[] that contains the widget ids of an application.
-     * */
+     * @param context     Context to use in the AppWidgetManager initialization.
+     * @param viewId      The id of the ImageView view that will load the image.
+     * @param width       Desired width of the bitmap that will be loaded.(Need to be manually set because of RemoteViews limitations.)
+     * @param height      Desired height of the bitmap that will be loaded. (Need to be manually set because of RemoteViews limitations.)
+     * @param widgetIds   The int[] that contains the widget ids of an application.
+     */
     public RemoteViewsTarget(@NonNull RemoteViews remoteViews,
                              @NonNull Context context,
                              @IdRes int viewId,
                              int width,
                              int height,
-                             int[] widgetIds) {
+                             @NonNull int... widgetIds) {
         this.mRemoteViews = remoteViews;
         this.mContext = context;
         this.mViewId = viewId;
@@ -54,14 +53,13 @@ public abstract class RemoteViewsTarget<Z> extends BaseTarget<Z> {
      * to get a handle on the Widget,
      * in order to update it.
      *
-     *
-     * @param remoteViews RemoteViews object which contains the ImageView that will load the bitmap.
-     * @param context Context to use in the AppWidgetManager initialization.
-     * @param viewId The id of the ImageView view that will load the image.
-     * @param width Desired width of the bitmap that will be loaded.(Need to be manually set because of RemoteViews limitations.)
-     * @param height Desired height of the bitmap that will be loaded. (Need to be manually set because of RemoteViews limitations.)
+     * @param remoteViews   RemoteViews object which contains the ImageView that will load the bitmap.
+     * @param context       Context to use in the AppWidgetManager initialization.
+     * @param viewId        The id of the ImageView view that will load the image.
+     * @param width         Desired width of the bitmap that will be loaded.(Need to be manually set because of RemoteViews limitations.)
+     * @param height        Desired height of the bitmap that will be loaded. (Need to be manually set because of RemoteViews limitations.)
      * @param componentName The ComponentName object that provides the handle to the Widget class.
-     * */
+     */
     public RemoteViewsTarget(@NonNull RemoteViews remoteViews,
                              @NonNull Context context,
                              @IdRes int viewId,
@@ -81,18 +79,18 @@ public abstract class RemoteViewsTarget<Z> extends BaseTarget<Z> {
      * Updates the Widget given a RemoteViews object.
      *
      * @param remoteViews The RemoteViews object of the Widget that we want to update.
-     * */
+     */
     private void update(RemoteViews remoteViews) {
-        if(this.mComponentName != null){
+        if (this.mComponentName != null) {
             mAppWidgetManager.updateAppWidget(this.mComponentName, remoteViews);
-        }else{
+        } else {
             mAppWidgetManager.updateAppWidget(this.mWidgetIds, remoteViews);
         }
     }
 
     @Override
     public void getSize(SizeReadyCallback cb) {
-        cb.onSizeReady(this.mWidth,this.mHeight);
+        cb.onSizeReady(this.mWidth, this.mHeight);
     }
 
     @Override
