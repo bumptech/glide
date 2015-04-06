2702cba8e4af72081787c02a2abc4f2e05e620bd
Sam Judd <judds@google.com>
2015-04-05 18:29:51 -0700

    Fix a code style error in ImageHeaderParser. Conflicts: 	library/src/main/java/com/bumptech/glide/load/resource/bitmap/ImageHeaderParser.java

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/ImageHeaderParser.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/ImageHeaderParser.java
@@ -403,27 +403,26 @@ public class ImageHeaderParser {
     @Override
     public long skip(long total) throws IOException {
       if (total < 0) {
-          return 0;
+        return 0;
       }
 
-      long skipped;
       long toSkip = total;
       while (toSkip > 0) {
-          skipped = is.skip(toSkip);
-          if (skipped > 0) {
-              toSkip -= skipped;
+        long skipped = is.skip(toSkip);
+        if (skipped > 0) {
+          toSkip -= skipped;
+        } else {
+          // Skip has no specific contract as to what happens when you reach the end of
+          // the stream. To differentiate between temporarily not having more data and
+          // having finished the stream, we read a single byte when we fail to skip any
+          // amount of data.
+          int testEofByte = is.read();
+          if (testEofByte == -1) {
+            break;
           } else {
-              // Skip has no specific contract as to what happens when you reach the end of
-              // the stream. To differentiate between temporarily not having more data and
-              // having finished the stream, we read a single byte when we fail to skip any
-              // amount of data.
-              int testEofByte = is.read();
-              if (testEofByte == -1) {
-                  break;
-              } else {
-                  toSkip--;
-              }
+            toSkip--;
           }
+        }
       }
       return total - toSkip;
     }
@@ -433,7 +432,7 @@ public class ImageHeaderParser {
       int toRead = buffer.length;
       int read;
       while (toRead > 0 && ((read = is.read(buffer, buffer.length - toRead, toRead)) != -1)) {
-          toRead -= read;
+        toRead -= read;
       }
       return buffer.length - toRead;
     }
