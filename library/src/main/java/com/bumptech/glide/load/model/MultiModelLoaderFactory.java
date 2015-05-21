2b0b775b52d0df1d658a9c2a04ad6cac715b7f23
judds <judds@google.com>
2015-05-21 15:23:10 -0700

    Synchronize access to MultiModelLoaderFactory.
    Fixes a concurrency bug where internal state meant to apply to a single
    build may be accessed by multiple builds simultaneously.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=94229486

--- library/src/main/java/com/bumptech/glide/load/model/MultiModelLoaderFactory.java
+++ library/src/main/java/com/bumptech/glide/load/model/MultiModelLoaderFactory.java
@@ -28,17 +28,18 @@ public class MultiModelLoaderFactory {
     this(context, DEFAULT_FACTORY);
   }
 
+  // Visible for testing.
   MultiModelLoaderFactory(Context context, Factory factory) {
     this.context = context.getApplicationContext();
     this.factory = factory;
   }
 
-  <Model, Data> void append(Class<Model> modelClass, Class<Data> dataClass,
+  synchronized <Model, Data> void append(Class<Model> modelClass, Class<Data> dataClass,
       ModelLoaderFactory<Model, Data> factory) {
     add(modelClass, dataClass, factory, true /*append*/);
   }
 
-  <Model, Data> void prepend(Class<Model> modelClass, Class<Data> dataClass,
+  synchronized <Model, Data> void prepend(Class<Model> modelClass, Class<Data> dataClass,
       ModelLoaderFactory<Model, Data> factory) {
     add(modelClass, dataClass, factory, false /*append*/);
   }
@@ -49,14 +50,14 @@ public class MultiModelLoaderFactory {
     entries.add(append ? entries.size() : 0, entry);
   }
 
-  <Model, Data> List<ModelLoaderFactory<Model, Data>> replace(Class<Model> modelClass,
+  synchronized <Model, Data> List<ModelLoaderFactory<Model, Data>> replace(Class<Model> modelClass,
       Class<Data> dataClass, ModelLoaderFactory<Model, Data> factory) {
     List<ModelLoaderFactory<Model, Data>> removed = remove(modelClass, dataClass);
     append(modelClass, dataClass, factory);
     return removed;
   }
 
-  <Model, Data> List<ModelLoaderFactory<Model, Data>> remove(Class<Model> modelClass,
+  synchronized <Model, Data> List<ModelLoaderFactory<Model, Data>> remove(Class<Model> modelClass,
       Class<Data> dataClass) {
     List<ModelLoaderFactory<Model, Data>> factories = new ArrayList<>();
     for (Iterator<Entry<?, ?>> iterator = entries.iterator(); iterator.hasNext(); ) {
@@ -69,7 +70,7 @@ public class MultiModelLoaderFactory {
     return factories;
   }
 
-  <Model> List<ModelLoader<Model, ?>> build(Class<Model> modelClass) {
+  synchronized <Model> List<ModelLoader<Model, ?>> build(Class<Model> modelClass) {
     List<ModelLoader<Model, ?>> loaders = new ArrayList<>();
     for (Entry<?, ?> entry : entries) {
       // Avoid stack overflow recursively creating model loaders by only creating loaders in
@@ -89,7 +90,7 @@ public class MultiModelLoaderFactory {
     return loaders;
   }
 
-  List<Class<?>> getDataClasses(Class<?> modelClass) {
+  synchronized List<Class<?>> getDataClasses(Class<?> modelClass) {
     List<Class<?>> result = new ArrayList<>();
     for (Entry<?, ?> entry : entries) {
       if (!result.contains(entry.dataClass) && entry.handles(modelClass)) {
@@ -99,7 +100,7 @@ public class MultiModelLoaderFactory {
     return result;
   }
 
-  public <Model, Data> ModelLoader<Model, Data> build(Class<Model> modelClass,
+  public synchronized <Model, Data> ModelLoader<Model, Data> build(Class<Model> modelClass,
       Class<Data> dataClass) {
     List<ModelLoader<Model, Data>> loaders = new ArrayList<>();
     boolean ignoredAnyEntries = false;
