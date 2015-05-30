3149acdcf6210c3c299ad1f88adc4be2de9ebf27
Sam Judd <judds@google.com>
2015-05-30 10:51:01 -0700

    Fix lint errors in flickr sample.

--- samples/flickr/src/main/java/com/bumptech/glide/samples/flickr/SquareImageView.java
+++ samples/flickr/src/main/java/com/bumptech/glide/samples/flickr/SquareImageView.java
@@ -1,6 +1,8 @@
 package com.bumptech.glide.samples.flickr;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -21,6 +23,7 @@ public final class SquareImageView extends ImageView {
     super(context, attrs, defStyleAttr);
   }
 
+  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
   public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
     super(context, attrs, defStyleAttr, defStyleRes);
   }
