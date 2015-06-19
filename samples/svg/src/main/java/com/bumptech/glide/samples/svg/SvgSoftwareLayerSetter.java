59fc0fcbd8fe491e33992481ef70399b3e52f7e2
Sam Judd <judds@google.com>
2015-06-19 08:28:34 -0700

    Add DataSource to RequestListener implementation in SVG sample.

--- samples/svg/src/main/java/com/bumptech/glide/samples/svg/SvgSoftwareLayerSetter.java
+++ samples/svg/src/main/java/com/bumptech/glide/samples/svg/SvgSoftwareLayerSetter.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.PictureDrawable;
 import android.os.Build;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.ImageViewTarget;
@@ -30,7 +31,7 @@ public class SvgSoftwareLayerSetter implements RequestListener<PictureDrawable> 
 
   @Override
   public boolean onResourceReady(PictureDrawable resource, Object model,
-      Target<PictureDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+      Target<PictureDrawable> target, DataSource dataSource, boolean isFirstResource) {
     ImageView view = ((ImageViewTarget<?>) target).getView();
     if (Build.VERSION_CODES.HONEYCOMB <= Build.VERSION.SDK_INT) {
       view.setLayerType(ImageView.LAYER_TYPE_SOFTWARE, null);
