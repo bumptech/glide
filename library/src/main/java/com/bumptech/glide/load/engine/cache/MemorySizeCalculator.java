9f550c2116ee1e33f2173e3999a28ca86146ef7c
Sam Judd <judds@google.com>
2015-08-15 11:01:25 -0700

    Don't rely on short-circuiting for sdk checks.
    Fixes #578.

--- library/src/main/java/com/bumptech/glide/load/engine/cache/MemorySizeCalculator.java
+++ library/src/main/java/com/bumptech/glide/load/engine/cache/MemorySizeCalculator.java
@@ -92,9 +92,11 @@ public class MemorySizeCalculator {
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     private static boolean isLowMemoryDevice(ActivityManager activityManager) {
-        final int sdkInt = Build.VERSION.SDK_INT;
-        return sdkInt < Build.VERSION_CODES.HONEYCOMB
-                || (sdkInt >= Build.VERSION_CODES.KITKAT && activityManager.isLowRamDevice());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return activityManager.isLowRamDevice();
+        } else {
+            return Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB;
+        }
     }
 
     private static class DisplayMetricsScreenDimensions implements ScreenDimensions {
