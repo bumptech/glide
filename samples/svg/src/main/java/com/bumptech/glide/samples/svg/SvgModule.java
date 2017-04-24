01795c8a09a2a24bcf655986db510fbc1be8e45b
Sam Judd <judds@google.com>
2017-04-24 09:19:25 -0700

    Fix SvgModule.

--- samples/svg/src/main/java/com/bumptech/glide/samples/svg/SvgModule.java
+++ samples/svg/src/main/java/com/bumptech/glide/samples/svg/SvgModule.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.drawable.PictureDrawable;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.RootGlideModule;
+import com.bumptech.glide.module.AppGlideModule;
 import com.caverock.androidsvg.SVG;
 import java.io.InputStream;
 
@@ -12,7 +12,7 @@ import java.io.InputStream;
  * Module for the SVG sample app.
  */
 @GlideModule
-public class SvgModule extends RootGlideModule {
+public class SvgModule extends AppGlideModule {
   @Override
   public void registerComponents(Context context, Registry registry) {
     registry.register(SVG.class, PictureDrawable.class, new SvgDrawableTranscoder())
