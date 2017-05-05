6c776104c0ccdbf9b0b218c9e92a06c0f08a69c4
Sam Judd <judds@google.com>
2017-05-05 07:36:42 -0700

    Include name in exclusion criteria for RequestOptions methods.
    Previously we were excluding only by type and 
    parameters, which excluded far more methods than
    just those that had been overridden by the 
    extension.

--- annotation/compiler/src/main/java/com/bumptech/glide/annotation/compiler/RequestOptionsGenerator.java
+++ annotation/compiler/src/main/java/com/bumptech/glide/annotation/compiler/RequestOptionsGenerator.java
@@ -577,8 +577,10 @@ final class RequestOptionsGenerator {
     private final TypeName returnType;
     private final List<TypeName> parameterTypes;
     private final Set<Modifier> modifiers;
+    private final String name;
 
     MethodSignature(MethodSpec spec) {
+      name = spec.name;
       modifiers = spec.modifiers;
       returnType = spec.returnType;
       parameterTypes =
@@ -596,7 +598,8 @@ final class RequestOptionsGenerator {
     public boolean equals(Object o) {
       if (o instanceof MethodSignature) {
         MethodSignature other = (MethodSignature) o;
-        return returnType.equals(other.returnType)
+        return name.equals(other.name)
+            && returnType.equals(other.returnType)
             && parameterTypes.equals(other.parameterTypes)
             && modifiers.equals(other.modifiers);
       }
@@ -605,7 +608,7 @@ final class RequestOptionsGenerator {
 
     @Override
     public int hashCode() {
-      return Objects.hashCode(returnType, parameterTypes, modifiers);
+      return Objects.hashCode(name, returnType, parameterTypes, modifiers);
     }
   }
 }
