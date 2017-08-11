a2a695f11e0a486337c8d0010a997ad536f6a565
loran <loran@google.com>
2017-08-11 11:27:51 -0700

    allow subclasses to read cache entry count.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=165002367

--- library/src/main/java/com/bumptech/glide/util/LruCache.java
+++ library/src/main/java/com/bumptech/glide/util/LruCache.java
@@ -56,6 +56,13 @@ public class LruCache<T, Y> {
   }
 
   /**
+   * Returns the number of entries stored in cache.
+   */
+  protected int getCount() {
+    return cache.size();
+  }
+
+  /**
    * A callback called whenever an item is evicted from the cache. Subclasses can override.
    *
    * @param key  The key of the evicted item.
