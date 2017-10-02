45013968bf451ac3219febd6f0794321916e62c0
asuszek <asuszek@google.com>
2017-10-02 16:39:29 -0700

    Do not use MediaStore thumbnail when size is SIZE_ORIGINAL.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=170774550

--- library/src/main/java/com/bumptech/glide/load/data/mediastore/MediaStoreUtil.java
+++ library/src/main/java/com/bumptech/glide/load/data/mediastore/MediaStoreUtil.java
@@ -3,6 +3,7 @@ package com.bumptech.glide.load.data.mediastore;
 import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.MediaStore;
+import com.bumptech.glide.request.target.Target;
 
 /**
  * Utility classes for interacting with the media store.
@@ -33,6 +34,9 @@ public final class MediaStoreUtil {
   }
 
   public static boolean isThumbnailSize(int width, int height) {
-    return width <= MINI_THUMB_WIDTH && height <= MINI_THUMB_HEIGHT;
+    return width != Target.SIZE_ORIGINAL
+        && height != Target.SIZE_ORIGINAL
+        && width <= MINI_THUMB_WIDTH
+        && height <= MINI_THUMB_HEIGHT;
   }
 }
