efa61f0d82519f5f40571fbc69f9bb81568e10b1
Sam Judd <judds@google.com>
2017-10-20 07:31:23 -0700

    Run application icon test against all installed applications with icons.

--- library/src/androidTest/java/com/bumptech/glide/test/TestNonBitmapResources.java
+++ library/src/androidTest/java/com/bumptech/glide/test/TestNonBitmapResources.java
@@ -4,9 +4,11 @@ import static com.google.common.truth.Truth.assertThat;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
@@ -15,6 +17,9 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import com.bumptech.glide.Glide;
 import com.google.common.collect.ImmutableList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Before;
@@ -23,6 +28,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class TestNonBitmapResources {
+  private static final ImmutableList<String> PACKAGES = ImmutableList.of(
+      "com.android.gallery3d",
+      "com.android.contacts");
 
   private Context context;
 
@@ -98,17 +106,22 @@ public class TestNonBitmapResources {
   }
 
   @Test
+  public void load_withNinePatchResourceId_asDrawable_producesNonNullDrawable()
+      throws ExecutionException, InterruptedException {
+    Drawable drawable = Glide.with(context)
+        .load(R.drawable.googlelogo_color_120x44dp)
+        .submit()
+        .get();
+
+    assertThat(drawable).isNotNull();
+  }
+
+  @Test
   public void load_withApplicationIconResourceIdUri_asDrawable_producesNonNullDrawable()
       throws NameNotFoundException, ExecutionException, InterruptedException {
-    ImmutableList<String> packages = ImmutableList.of(
-        "com.google.android.apps.photos",
-        "com.google.android.apps.messaging"
-    );
-    PackageManager packageManager = context.getPackageManager();
-    for (String packageName : packages) {
-      PackageInfo packageInfo =
-          packageManager.getPackageInfo(packageName, /*flags=*/ 0);
-      int iconResourceId = packageInfo.applicationInfo.icon;
+    for (String packageName : getInstalledPackages()) {
+      int iconResourceId = getResourceId(packageName);
+
       Uri uri = new Uri.Builder()
           .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
           .authority(packageName)
@@ -124,28 +137,10 @@ public class TestNonBitmapResources {
   }
 
   @Test
-  public void load_withNinePatchResourceId_asDrawable_producesNonNullDrawable()
-      throws ExecutionException, InterruptedException {
-    Drawable drawable = Glide.with(context)
-        .load(R.drawable.googlelogo_color_120x44dp)
-        .submit()
-        .get();
-
-    assertThat(drawable).isNotNull();
-  }
-
-  @Test
   public void load_withApplicationIconResourceNameUri_asDrawable_producesNonNullDrawable()
       throws ExecutionException, InterruptedException, NameNotFoundException {
-     ImmutableList<String> packages = ImmutableList.of(
-        "com.google.android.apps.photos",
-        "com.google.android.apps.messaging"
-    );
-    PackageManager packageManager = context.getPackageManager();
-    for (String packageName : packages) {
-      PackageInfo packageInfo =
-          packageManager.getPackageInfo(packageName, /*flags=*/ 0);
-      int iconResourceId = packageInfo.applicationInfo.icon;
+    for (String packageName : getInstalledPackages()) {
+      int iconResourceId = getResourceId(packageName);
 
       Context toUse = context.createPackageContext(packageName, /*flags=*/ 0);
       Resources resources = toUse.getResources();
@@ -163,5 +158,31 @@ public class TestNonBitmapResources {
           .get();
       assertThat(drawable).isNotNull();
     }
+  }
+
+  private Set<String> getInstalledPackages() {
+    Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+    mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+    PackageManager packageManager = context.getPackageManager();
+    List<ResolveInfo> pkgAppsList =
+        packageManager.queryIntentActivities(mainIntent, /*flags=*/ 0);
+    Set<String> result = new HashSet<>();
+    for (ResolveInfo info : pkgAppsList) {
+      int iconResourceId = getResourceId(info.activityInfo.packageName);
+      if (iconResourceId != 0) {
+        result.add(info.activityInfo.packageName);
+      }
+    }
+    return result;
+  }
+
+  private int getResourceId(String packageName) {
+    PackageInfo packageInfo;
+    try {
+      packageInfo = context.getPackageManager().getPackageInfo(packageName, /*flags=*/ 0);
+    } catch (NameNotFoundException e) {
+      return 0;
+    }
+    return packageInfo.applicationInfo.icon;
   }
 }
