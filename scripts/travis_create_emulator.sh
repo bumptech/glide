cb1276d53eea4cf1b69c65cf807e84a51e0db79d
Sam Judd <judds@google.com>
2017-10-20 19:19:32 -0700

    Only install emulators if they’re used by the build matrix.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo y | android --silent update sdk --no-ui --all --filter android-$ANDROID_TARGET
+echo y | android --silent update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-$ANDROID_TARGET
 echo no | android create avd --force -n test -t android-$ANDROID_TARGET --abi armeabi-v7a
 QEMU_AUDIO_DRV=none emulator -engine classic -avd test -no-window &
 
