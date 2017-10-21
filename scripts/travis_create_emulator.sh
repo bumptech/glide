29992732cce5e3f2979f80adab9defd35b71c3f6
Sam Judd <judds@google.com>
2017-10-20 19:06:57 -0700

    Run API 16 emulator as well as API 22.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-$ANDROID_TARGET --abi armeabi-v7a
 QEMU_AUDIO_DRV=none emulator -engine classic -avd test -no-window &
 
 exit 0
