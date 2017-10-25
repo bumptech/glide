91682c90ca064b03a12c4618c652920dbc5305e7
Sam Judd <judds@google.com>
2017-10-24 19:37:52 -0700

    Add more travis emulator tests.

--- scripts/travis_instrumentation.sh
+++ scripts/travis_instrumentation.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ $ANDROID_TARGET -gt 18 ]; then
+  echo "Emulator tests >= 19 are run on Firebase ignoring"
+  exit 0
+fi
+
+echo "Starting emulator for $COMPONENT tests"
+./scripts/travis_create_emulator.sh &
+
 ./gradlew :instrumentation:assembleDebug :instrumentation:assembleDebugAndroidTest --parallel
 
 echo "Waiting for emulator..."
