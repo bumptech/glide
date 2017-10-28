f8bd27cec5dfd9d308219f2d97ef3f1a8abc6419
Sam Judd <judds@google.com>
2017-10-27 17:49:34 -0700

    Run emulator tests on all available Android versions on Firebase.

--- scripts/travis_firebase.sh
+++ scripts/travis_firebase.sh
@@ -17,10 +17,12 @@ apk_dir=instrumentation/build/outputs/apk
   --type instrumentation \
   --app $apk_dir/instrumentation-debug.apk \
   --test $apk_dir/instrumentation-debug-androidTest.apk \
-  --device model=Nexus6P,version=26,locale=en,orientation=portrait  \
+  --device model=Nexus6P,version=26,locale=en,orientation=portrait \
   --device model=Nexus6P,version=25,locale=en,orientation=portrait \
+  --device model=Nexus6P,version=24,locale=en,orientation=portrait \
   --device model=Nexus6P,version=23,locale=en,orientation=portrait \
   --device model=Nexus6,version=22,locale=en,orientation=portrait \
+  --device model=Nexus5,version=21,locale=en,orientation=portrait \
   --device model=Nexus5,version=19,locale=en,orientation=portrait \
   --project android-glide \
   --no-auto-google-login \
