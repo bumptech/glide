5d74999a49a1f41777c1371088b7a939b807ed2b
Kevin Read <obsidian17@gmail.com>
2017-11-07 23:22:17 +0100

    Use Iterator.remove when trimming cache  (#2550)
    * Use Iterator.remove when trimming cache so even keys violating equals() can be used here.
    Fixes #2532
    
    * Ensure that we don't overrun the iterator
    
    * Get new iterator for each iteration so concurrent modification is possible. #2532

--- library/src/main/java/com/bumptech/glide/util/LruCache.java
+++ library/src/main/java/com/bumptech/glide/util/LruCache.java
@@ -1,6 +1,8 @@
 package com.bumptech.glide.util;
 
 import android.support.annotation.Nullable;
+
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -166,12 +168,14 @@ public class LruCache<T, Y> {
    */
   protected synchronized void trimToSize(long size) {
     Map.Entry<T, Y> last;
+    Iterator<Map.Entry<T, Y>> cacheIterator;
     while (currentSize > size) {
-      last = cache.entrySet().iterator().next();
+      cacheIterator  = cache.entrySet().iterator();
+      last = cacheIterator.next();
       final Y toRemove = last.getValue();
       currentSize -= getSize(toRemove);
       final T key = last.getKey();
-      cache.remove(key);
+      cacheIterator.remove();
       onItemEvicted(key, toRemove);
     }
   }
