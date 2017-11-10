46e4fc70770c5b7e7787c7fb8e3d8aab27459bd2
Sam Judd <judds@google.com>
2017-11-10 09:28:57 -0800

    Remove a couple of unused imports in Glide's compiler extension test resources.

--- annotation/compiler/test/src/test/resources/GlideExtensionWithTypeTest/ExtensionWithType.java
+++ annotation/compiler/test/src/test/resources/GlideExtensionWithTypeTest/ExtensionWithType.java
@@ -2,9 +2,7 @@ package com.bumptech.glide.test;
 
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.annotation.GlideExtension;
-import com.bumptech.glide.annotation.GlideOption;
 import com.bumptech.glide.annotation.GlideType;
-import com.bumptech.glide.request.RequestOptions;
 
 @GlideExtension
 public final class ExtensionWithType {
