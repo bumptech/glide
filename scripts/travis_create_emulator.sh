589597481028abf68a90526316dbdb846c1465f4
Sam Judd <judds@google.com>
2017-11-13 18:32:46 -0800

    Set emulator memory to 2048.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -5,7 +5,7 @@ set -e
 target="system-images;android-${ANDROID_TARGET};default;armeabi-v7a"
 echo y | sdkmanager --update
 echo y | sdkmanager --install $target
-avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 2000M 
-QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window &
+avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 2000M
+QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 &
 
 exit 0
