7159c7b2fe61d453257c01348fa54c6e6dc22743
Sam Judd <judds@google.com>
2017-11-14 22:56:26 -0800

    Enable hardware Bitmaps in Flickr sample.

--- samples/flickr/src/main/java/com/bumptech/glide/samples/flickr/FlickrGlideModule.java
+++ samples/flickr/src/main/java/com/bumptech/glide/samples/flickr/FlickrGlideModule.java
@@ -2,9 +2,12 @@ package com.bumptech.glide.samples.flickr;
 
 import android.content.Context;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.samples.flickr.api.Photo;
 import java.io.InputStream;
 
@@ -13,6 +16,13 @@ import java.io.InputStream;
  */
 @GlideModule
 public class FlickrGlideModule extends AppGlideModule {
+
+  @Override
+  public void applyOptions(Context context, GlideBuilder builder) {
+    super.applyOptions(context, builder);
+    builder.setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_ARGB_8888));
+  }
+
   @Override
   public void registerComponents(Context context, Glide glide, Registry registry) {
     registry.append(Photo.class, InputStream.class, new FlickrModelLoader.Factory());
