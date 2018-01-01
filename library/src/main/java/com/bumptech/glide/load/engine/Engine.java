7114691a8b00c3013f5b7d0642714d321aa6bfc9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-01-01 14:42:54 +0000

    Revert "Fix spurious PMD warnings when building with —-profile."
    This reverts commit cc4bb160c555e90c354f5c4c9c2722756709232f.

--- library/src/main/java/com/bumptech/glide/load/engine/Engine.java
+++ library/src/main/java/com/bumptech/glide/load/engine/Engine.java
@@ -235,14 +235,10 @@ public class Engine implements EngineJobListener,
     return new LoadStatus(cb, engineJob);
   }
 
-  // TODO(#2776): Remove this suppression.
-  @SuppressWarnings("PMD.UnusedPrivateMethod")
   private static void logWithTimeAndKey(String log, long startTime, Key key) {
     Log.v(TAG, log + " in " + LogTime.getElapsedMillis(startTime) + "ms, key: " + key);
   }
 
-  // TODO(#2776): Remove this suppression.
-  @SuppressWarnings("PMD.UnusedPrivateMethod")
   @Nullable
   private EngineResource<?> loadFromActiveResources(Key key, boolean isMemoryCacheable) {
     if (!isMemoryCacheable) {
@@ -256,8 +252,6 @@ public class Engine implements EngineJobListener,
     return active;
   }
 
-  // TODO(#2776): Remove this suppression.
-  @SuppressWarnings("PMD.UnusedPrivateMethod")
   private EngineResource<?> loadFromCache(Key key, boolean isMemoryCacheable) {
     if (!isMemoryCacheable) {
       return null;
