6e932b374e3f3b87a5f105f27f89767e82d68129
Sam Judd <judds@google.com>
2018-03-30 09:21:59 -0700

    Fix a few errors in the BitmapTransformation javadoc.

--- library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
+++ library/src/main/java/com/bumptech/glide/load/resource/bitmap/BitmapTransformation.java
@@ -22,7 +22,7 @@ import java.security.MessageDigest;
  * <code>
  * public class FillSpace extends BitmapTransformation {
  *     private static final String ID = "com.bumptech.glide.transformations.FillSpace";
- *     private static final String ID_BYTES = ID.getBytes(STRING_CHARSET_NAME);
+ *     private static final byte[] ID_BYTES = ID.getBytes(Charset.forName("UTF-8"));
  *
  *     {@literal @Override}
  *     public Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
@@ -34,7 +34,7 @@ import java.security.MessageDigest;
  *     }
  *
  *     {@literal @Override}
- *     public void equals(Object o) {
+ *     public boolean equals(Object o) {
  *       return o instanceof FillSpace;
  *     }
  *
@@ -44,8 +44,7 @@ import java.security.MessageDigest;
  *     }
  *
  *     {@literal @Override}
- *     public void updateDiskCacheKey(MessageDigest messageDigest)
- *         throws UnsupportedEncodingException {
+ *     public void updateDiskCacheKey(MessageDigest messageDigest) {
  *       messageDigest.update(ID_BYTES);
  *     }
  * }
