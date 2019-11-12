a7492f9439124f592a8458c2d6d54aa25a52abe6
judds <judds@google.com>
2019-11-12 09:34:31 -0800

    Avoid catching ErrnoException directly in ParcelFileDescripotorRewinder
    ErrnoException requires API level 21. Caught exceptions are apparently
    initialized at class load time, rather than at runtime. As a result, any
    usage of the class will fail on older API levels. Guarding the specific
    block of code is insufficient.
    
    To preserve the external API and meet the OS requirement, I've moved the
    logic that deals with Os.lseek into a static inner class that shouldn't
    need to be initialized until an instance of ParcelFileDescriptorRewinder
    is actually created. Since we guard creation of
    ParcelFileDescriptorRewinder with #isSupported(), we should be safe.
    
    PiperOrigin-RevId: 279987765

--- library/src/main/java/com/bumptech/glide/load/data/ParcelFileDescriptorRewinder.java
+++ library/src/main/java/com/bumptech/glide/load/data/ParcelFileDescriptorRewinder.java
@@ -2,12 +2,12 @@ package com.bumptech.glide.load.data;
 
 import static android.system.OsConstants.SEEK_SET;
 
-import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.ParcelFileDescriptor;
 import android.system.ErrnoException;
 import android.system.Os;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import java.io.IOException;
 
 /**
@@ -15,33 +15,26 @@ import java.io.IOException;
  */
 public final class ParcelFileDescriptorRewinder implements DataRewinder<ParcelFileDescriptor> {
 
-  private final ParcelFileDescriptor parcelFileDescriptor;
+  private final InternalRewinder rewinder;
 
   public static boolean isSupported() {
     // Os.lseek() is only supported on API 21+.
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
   }
 
+  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   public ParcelFileDescriptorRewinder(ParcelFileDescriptor parcelFileDescriptor) {
     if (!isSupported()) {
       throw new UnsupportedOperationException("This class should only be instantiated on L+");
     }
-    this.parcelFileDescriptor = parcelFileDescriptor;
+    rewinder = new InternalRewinder(parcelFileDescriptor);
   }
 
-  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   @NonNull
   @Override
   public ParcelFileDescriptor rewindAndGet() throws IOException {
-    try {
-      // This will probably fail for pipes, but in that case Glide loading will fall back to
-      // InputStreams, which should handle most cases.
-      Os.lseek(parcelFileDescriptor.getFileDescriptor(), 0, SEEK_SET);
-    } catch (ErrnoException e) {
-      throw new IOException("Unable to rewind", e);
-    }
-
-    return parcelFileDescriptor;
+    return rewinder.rewind();
   }
 
   @Override
@@ -52,6 +45,7 @@ public final class ParcelFileDescriptorRewinder implements DataRewinder<ParcelFi
   /**
    * Factory for producing {@link ParcelFileDescriptorRewinder}s from {@link ParcelFileDescriptor}s.
    */
+  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
   public static final class Factory implements DataRewinder.Factory<ParcelFileDescriptor> {
 
     @NonNull
@@ -65,6 +59,24 @@ public final class ParcelFileDescriptorRewinder implements DataRewinder<ParcelFi
     @Override
     public Class<ParcelFileDescriptor> getDataClass() {
       return ParcelFileDescriptor.class;
+    }
+  }
+
+  @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+  private static final class InternalRewinder {
+    private final ParcelFileDescriptor parcelFileDescriptor;
+
+    InternalRewinder(ParcelFileDescriptor parcelFileDescriptor) {
+      this.parcelFileDescriptor = parcelFileDescriptor;
+    }
+
+    ParcelFileDescriptor rewind() throws IOException {
+      try {
+        Os.lseek(parcelFileDescriptor.getFileDescriptor(), 0, SEEK_SET);
+      } catch (ErrnoException e) {
+        throw new IOException(e);
+      }
+      return parcelFileDescriptor;
     }
   }
 }
