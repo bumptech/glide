d505900a16f93db8daf2d073ea5b451b5d908fc1
Danny7226 <dchai2@uci.edu>
2020-03-17 10:59:53 -0700

    revise test case name, use lambda interface indicating NPE throw

--- library/test/src/test/java/com/bumptech/glide/load/model/StreamEncoderTest.java
+++ library/test/src/test/java/com/bumptech/glide/load/model/StreamEncoderTest.java
@@ -2,6 +2,7 @@ package com.bumptech.glide.load.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import com.bumptech.glide.load.Options;
@@ -10,9 +11,11 @@ import com.bumptech.glide.util.ByteBufferUtil;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Executable;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
@@ -50,24 +53,32 @@ public class StreamEncoderTest {
     assertEquals(fakeData, new String(data, "UTF-8"));
   }
 
-  @Test(expected = NullPointerException.class)
+  @Test
   public void testFileNullException() throws IOException {
     String fakeData = "SomeRandomFakeData";
-    ByteArrayInputStream is = new ByteArrayInputStream(fakeData.getBytes("UTF-8"));
-    boolean success = false;
+    final ByteArrayInputStream is = new ByteArrayInputStream(fakeData.getBytes("UTF-8"));
+    final boolean[] success = {false};
+
+      class Test implements ThrowingRunnable{
+        @Override
+        public void run() throws Throwable {
+          success[0] = encoder.encode(is, null, new Options());
+        }
+      }
 
     // Write some data to #file making sure file.delete() executes properly
     encoder.encode(is, file, new Options());
 
-    success = encoder.encode(is, null, new Options());
-    // method should end here, since null file exception
 
-    // If #encoder.encode() does not throw exception, encoding process should be successful, assert true
-    assertTrue(success);
+    // Assert a NullPointerException is thrown here.
+    assertThrows(NullPointerException.class, new Test());
+
+    // Assert encoding process failed.
+    assertFalse(success[0]);
   }
 
   @Test
-  public void testImportanceOfOptionsArgument() throws IOException {
+  public void encode_withNullOptions_completesSuccessfully() throws IOException {
     // testing the importance of Options argument in #encode() method
     String fakeData = "SomeRandomFakeData";
     ByteArrayInputStream is = new ByteArrayInputStream(fakeData.getBytes("UTF-8"));
