de73024872dfde14026ecc6eed6bcc6792f54b3b
judds <judds@google.com>
2020-06-23 15:27:31 -0700

    Avoid adding multiple pre draw listeners in Glide.
    PiperOrigin-RevId: 317950388

--- library/src/main/java/com/bumptech/glide/manager/FirstFrameWaiter.java
+++ library/src/main/java/com/bumptech/glide/manager/FirstFrameWaiter.java
@@ -1,8 +1,14 @@
 package com.bumptech.glide.manager;
 
 import android.app.Activity;
+import android.view.View;
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 final class FirstFrameWaiter {
+  private final Set<View> pendingDecorViews =
+      Collections.newSetFromMap(new WeakHashMap<View, Boolean>());
 
   void registerSelf(Activity activity) {}
 }
