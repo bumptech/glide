dfefa5c1f906ff241b9c374783e8fc1a11b474e3
Jakub Juszczyk <jakub.juszczyk@flipfit.com>
2020-10-08 15:20:40 +0200

    Bounce option

--- third_party/gif_decoder/src/main/java/com/bumptech/glide/gifdecoder/StandardGifDecoder.java
+++ third_party/gif_decoder/src/main/java/com/bumptech/glide/gifdecoder/StandardGifDecoder.java
@@ -106,7 +106,8 @@ public class StandardGifDecoder implements GifDecoder {
   private int[] mainScratch;
 
   private int framePointer;
-  private Boolean frameBackward;
+  private boolean bounce = false;
+  private boolean frameBackward = false;
   private GifHeader header;
   private Bitmap previousImage;
   private boolean savePrevious;
@@ -131,6 +132,14 @@ public class StandardGifDecoder implements GifDecoder {
       @NonNull GifDecoder.BitmapProvider provider, GifHeader gifHeader, ByteBuffer rawData,
       int sampleSize) {
     this(provider);
+    setData(gifHeader, rawData, sampleSize);
+  }
+
+  public StandardGifDecoder(
+      @NonNull GifDecoder.BitmapProvider provider, GifHeader gifHeader, ByteBuffer rawData,
+      int sampleSize, boolean bounce) {
+    this(provider);
+    this.bounce = bounce;
     setData(gifHeader, rawData, sampleSize);
   }
 
@@ -163,13 +172,16 @@ public class StandardGifDecoder implements GifDecoder {
 
   @Override
   public void advance() {
-    framePointer = (framePointer + (frameBackward == true ? -1 : 1)) % header.frameCount;
-    
-    if (framePointer == header.frameCount - 1) {
-      frameBackward = true;
-    }
-    if (framePointer == 0) {
-      frameBackward = false;
+    int nextPointer = bounce && frameBackward && framePointer > 0 ? -1 : 1;
+    framePointer = (framePointer + nextPointer) % header.frameCount;
+
+    if (bounce) {
+      if (framePointer == header.frameCount - 1) {
+        frameBackward = true;
+      }
+      if (framePointer == 0) {
+        frameBackward = false;
+      }
     }
   }
 
