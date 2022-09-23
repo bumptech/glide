dafc7e21d3d4cad5cc257dc39bb09cfb9b651106
Sam Judd <judds@google.com>
2022-09-23 13:48:04 -0700

    Avoid a javadoc tag warning in GlideModule
    PiperOrigin-RevId: 476453614

--- annotation/src/main/java/com/bumptech/glide/annotation/GlideModule.java
+++ annotation/src/main/java/com/bumptech/glide/annotation/GlideModule.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Identifies AppGlideModules and LibraryGlideModules for Glide's annotation processor to merge at
  * compile time.
  *
- * <p>Replaces <meta-data /> tags in AndroidManifest.xml.
+ * <p>Replaces {@code <meta-data />} tags in AndroidManifest.xml.
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
