89472a35df81ca8924ad31870ccd17a5a800674e
Sam Judd <judds@google.com>
2022-11-06 19:07:04 -0800

    Apply optional transformations for ContentScales
    Using optional trasnformations more closely matches the behavior of
    RequestBuilder when loading into Views.
    
    Using non-optional transformations will cause exceptions to be thrown if
    the loaded resource type cannot be transformed. In particular any
    non-Drawable / non-Bitmap resource will throw as will any Drawable type
    that cannot be converted into a simple Bitmap.
    
    Fixes #4943

--- integration/compose/src/androidTest/java/com/bumptech/glide/integration/compose/GlideImageCustomDrawableTransformationTest.kt
+++ integration/compose/src/androidTest/java/com/bumptech/glide/integration/compose/GlideImageCustomDrawableTransformationTest.kt
@@ -1,4 +1,100 @@
+@file:OptIn(ExperimentalGlideComposeApi::class, ExperimentalCoroutinesApi::class)
+
 package com.bumptech.glide.integration.compose
 
-class GlideImageCustomDrawableTransformationTest {
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ScaleFactor
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.test.Constants
+import com.bumptech.glide.integration.compose.test.GlideComposeRule
+import com.bumptech.glide.integration.compose.test.assertDisplaysInstance
+import com.bumptech.glide.integration.compose.test.onNodeWithDefaultContentDescription
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+/**
+ * Tests Issue #4943.
+ *
+ * Transformable types are tested in [GlideImageDefaultTransformationTest].
+ */
+@RunWith(Parameterized::class)
+class GlideImageCustomDrawableTransformationTest(private val contentScale: ContentScale) {
+  @get:Rule val glideComposeRule = GlideComposeRule()
+
+  @Test
+  fun glideImage_nonBitmapDrawable_doesNotThrow() = runTest {
+    val customDrawable = FakeDrawable()
+
+    glideComposeRule.setContent {
+      GlideImage(
+        model = customDrawable,
+        contentScale = contentScale,
+        contentDescription = Constants.DEFAULT_DESCRIPTION,
+        modifier = Modifier.size(100.dp, 200.dp)
+      )
+    }
+
+    glideComposeRule.onNodeWithDefaultContentDescription().assertDisplaysInstance(customDrawable)
+  }
+
+  @Test
+  fun glideImage_animatableDrawable_doesNotThrow() = runTest {
+    val customDrawable = FakeAnimatableDrawable()
+
+    glideComposeRule.setContent {
+      GlideImage(
+        model = customDrawable,
+        contentScale = contentScale,
+        contentDescription = Constants.DEFAULT_DESCRIPTION,
+        modifier = Modifier.size(200.dp, 100.dp)
+      )
+    }
+
+    glideComposeRule.onNodeWithDefaultContentDescription().assertDisplaysInstance(customDrawable)
+  }
+
+  companion object {
+    @JvmStatic
+    @Parameterized.Parameters(name = "{0}: contentScale")
+    fun data() = arrayOf(
+      ContentScale.Crop,
+      ContentScale.FillBounds,
+      ContentScale.FillHeight,
+      ContentScale.FillWidth,
+      ContentScale.Fit,
+      ContentScale.Inside,
+      ContentScale.None,
+      object : ContentScale {
+        override fun computeScaleFactor(srcSize: Size, dstSize: Size): ScaleFactor =
+          ContentScale.Fit.computeScaleFactor(srcSize, dstSize)
+      },
+    )
+  }
+}
+
+@Suppress("DeprecatedCallableAddReplaceWith")
+private open class FakeDrawable : Drawable() {
+  override fun draw(p0: Canvas) {
+  }
+  override fun setAlpha(p0: Int) = throw UnsupportedOperationException()
+  override fun setColorFilter(p0: ColorFilter?) = throw UnsupportedOperationException()
+  @Deprecated("Deprecated in Java")
+  override fun getOpacity(): Int = throw UnsupportedOperationException()
+}
+
+private class FakeAnimatableDrawable : FakeDrawable(), Animatable {
+  override fun start() {}
+  override fun stop() {}
+  override fun isRunning(): Boolean = throw UnsupportedOperationException()
 }
--- integration/compose/src/androidTest/java/com/bumptech/glide/integration/compose/test/nodes.kt
+++ integration/compose/src/androidTest/java/com/bumptech/glide/integration/compose/test/nodes.kt
@@ -1,2 +1,20 @@
 package com.bumptech.glide.integration.compose.test
 
+import android.graphics.drawable.Drawable
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+
+object Constants {
+  const val DEFAULT_DESCRIPTION = "test"
+}
+
+fun ComposeContentTestRule.onNodeWithDefaultContentDescription() =
+  onNodeWithContentDescription(Constants.DEFAULT_DESCRIPTION)
+
+fun SemanticsNodeInteraction.assertDisplays(drawable: Drawable) =
+  assert(expectDisplayedDrawable(drawable))
+
+fun SemanticsNodeInteraction.assertDisplaysInstance(drawable: Drawable) =
+  assert(expectSameInstance(drawable))
