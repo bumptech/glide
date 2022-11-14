e63c5d216520baecaaad9273699765b8c02b28be
Sam Judd <judds@google.com>
2022-11-13 16:11:13 -0800

    Change Glide's preloading API to be data based
    By data based we mean that we preload based on access to the data,
    rather than the position of the user in the view. Data based access
    matches how the Compose paging API works and is much simpler to
    implement in safe / reasonable way. The primary compromise is that we
    cannot begin preloading until the data is accessed. If there are
    multiple disjoint data sets or multiple header rows, we may not preload
    data while scrolling until we reach the first item in each data set.
    Users can work around this in each application by manually accessing the
    prealoding data set, but it's not super convenient.
    
    Another option would be to wrap LazyListState and track each insertion
    via item / items. That would let our preloader track exactly which
    positions are preloadable and which are not. While the functionality is
    somewhat better and it's safe, the implementation is much larger and it's
    more complex for callers to call correctly. We can add it in later if we
    find that the data based API is painful to use.
    
    Yet another option would be to more faithfully implement the
    ListPreloader interface and rely on users to paper over more gaps, just
    as we do for non-Compose code. This is significantly more complex for
    users to implement.
    
    We've discussed a few options for this API here:
    https://chat.google.com/room/AAAAYRnp4-Y/Q61ILNtb8hu

--- samples/gallery/src/main/java/com/bumptech/glide/samples/gallery/HorizontalGalleryFragment.kt
+++ samples/gallery/src/main/java/com/bumptech/glide/samples/gallery/HorizontalGalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.bumptech.glide.samples.gallery
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,22 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
-import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestManager
+import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.integration.compose.GlideLazyListPreloader
+import com.bumptech.glide.integration.compose.rememberGlidePreloadingData
 import com.bumptech.glide.signature.MediaStoreSignature
 
 /** Displays media store data in a recycler view. */
@@ -48,51 +44,37 @@ class HorizontalGalleryFragment : Fragment() {
 
   @Composable
   fun DeviceMedia(mediaStoreData: List<MediaStoreData>) {
-    val state = rememberLazyListState()
-    val requestManager = rememberRequestManager()
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(10.dp), state = state) {
-      items(mediaStoreData) { mediaStoreItem ->
-        MediaStoreView(mediaStoreItem, requestManager, Modifier.fillParentMaxSize())
+    val requestBuilderTransform =
+      { item: MediaStoreData, requestBuilder: RequestBuilder<Drawable> ->
+        requestBuilder.load(item.uri).signature(item.signature())
+      }
+
+    val preloadingData =
+      rememberGlidePreloadingData(
+        mediaStoreData,
+        THUMBNAIL_SIZE,
+        requestBuilderTransform = requestBuilderTransform,
+      )
+
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+      items(preloadingData.size) { index ->
+        val (mediaStoreItem, preloadRequestBuilder) = preloadingData[index]
+        MediaStoreView(mediaStoreItem, preloadRequestBuilder, Modifier.fillParentMaxSize())
       }
     }
-
-    GlideLazyListPreloader(
-      state = state,
-      data = mediaStoreData,
-      size = THUMBNAIL_SIZE,
-      numberOfItemsToPreload = 15,
-      fixedVisibleItemCount = 2,
-    ) { item, requestBuilder ->
-      requestBuilder.load(item.uri).signature(item.signature())
-    }
   }
-
-  @Composable
-  private fun rememberRequestManager() =
-    LocalContext.current.let { remember(it) { Glide.with(it) } }
 
   private fun MediaStoreData.signature() = MediaStoreSignature(mimeType, dateModified, orientation)
 
   @Composable
-  fun MediaStoreView(item: MediaStoreData, requestManager: RequestManager, modifier: Modifier) {
-    val signature = item.signature()
-
-    GlideImage(
-      model = item.uri,
-      contentDescription = item.displayName,
-      modifier = modifier,
-    ) {
-      it
-        .thumbnail(
-          requestManager
-            .asDrawable()
-            .load(item.uri)
-            .signature(signature)
-            .override(THUMBNAIL_DIMENSION)
-        )
-        .signature(signature)
+  fun MediaStoreView(
+    item: MediaStoreData,
+    preloadRequestBuilder: RequestBuilder<Drawable>,
+    modifier: Modifier,
+  ) =
+    GlideImage(model = item.uri, contentDescription = item.displayName, modifier = modifier) {
+      it.thumbnail(preloadRequestBuilder).signature(item.signature())
     }
-  }
 
   companion object {
     private const val THUMBNAIL_DIMENSION = 50
