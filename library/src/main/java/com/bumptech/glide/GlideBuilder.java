fe212a60ea95f2555b0f42fced6ca6ba6b374c65
Sam <sam.a.judd@gmail.com>
2023-04-03 15:46:20 -0700

    Hard code disabling hardware bitmaps on O/OMR1. (#5115)

--- library/src/main/java/com/bumptech/glide/GlideBuilder.java
+++ library/src/main/java/com/bumptech/glide/GlideBuilder.java
@@ -488,15 +488,11 @@ public final class GlideBuilder {
   }
 
   /**
-   * Disables hardware bitmaps if the sdk level is <= O and {@code disableHardwareBitmapsOnO} is
-   * {@code true}.
-   *
-   * @deprecated This method is experimental. It will be hard coded and removed in a future release
+   * @deprecated This method does nothing. It will be hard coded and removed in a future release
    *     without further warning.
    */
   @Deprecated
   public GlideBuilder setDisableHardwareBitmapsOnO(boolean disableHardwareBitmapsOnO) {
-    glideExperimentsBuilder.add(new DisableHardwareBitmapsOnO(disableHardwareBitmapsOnO));
     return this;
   }
 
@@ -593,14 +589,6 @@ public final class GlideBuilder {
         manifestModules,
         annotationGeneratedGlideModule,
         experiments);
-  }
-
-  static final class DisableHardwareBitmapsOnO implements Experiment {
-    final boolean disableHardwareBitmapsOnO;
-
-    DisableHardwareBitmapsOnO(boolean disableHardwareBitmapsOnO) {
-      this.disableHardwareBitmapsOnO = disableHardwareBitmapsOnO;
-    }
   }
 
   static final class ManualOverrideHardwareBitmapMaxFdCount implements Experiment {
