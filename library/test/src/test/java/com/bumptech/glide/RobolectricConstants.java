cd98fa3f7d6daa321d50ac830998d1a76064ff21
Brett Chabot <brettchabot@google.com>
2023-07-06 18:36:40 -0700

    Increment ROBOLECTRIC_SDK to 19 from 18.
    A future version of Robolectric will drop support
    for SDK 18.
    
    Tested via gradle test testDebugUnitTest.

--- library/test/src/test/java/com/bumptech/glide/RobolectricConstants.java
+++ library/test/src/test/java/com/bumptech/glide/RobolectricConstants.java
@@ -2,5 +2,5 @@ package com.bumptech.glide;
 
 public class RobolectricConstants {
   /** The default SDK used for Robolectric tests */
-  public static final int ROBOLECTRIC_SDK = 18;
+  public static final int ROBOLECTRIC_SDK = 19;
 }
