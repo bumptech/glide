5468a24d51a1bca79857814ce59e3b5695dc0cc8
Sam Judd <sam.a.judd@gmail.com>
2023-09-02 13:13:45 -0700

    Add a goldens based test utility for compose

---
+++ integration/compose/src/androidTest/java/com/bumptech/glide/test/goldens.kt
@@ -0,0 +1,95 @@
+package com.bumptech.glide.test
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Environment
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toPixelMap
+import androidx.test.core.app.ApplicationProvider
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.lang.IllegalStateException
+
+const val GENERATED_FILES_DIR = "compose_goldens"
+const val EXTENSION = "png"
+
+fun ImageBitmap.compareToGolden(testName: String) {
+  val bitmap = toBitmap()
+  val existingGolden = readExistingGolden(testName)
+  val reasonForWrite = if (existingGolden == null) {
+    "Missing golden"
+  } else if (!existingGolden.sameAs(bitmap)) {
+    "Different golden"
+  } else {
+    null
+  }
+  if (reasonForWrite != null) {
+    val filePath = writeBitmap(bitmap, testName)
+    throw IllegalStateException("$reasonForWrite for $testName, wrote a new one. cd to androidTest/assets and run: adb pull $filePath")
+  }
+}
+
+private fun ImageBitmap.toBitmap(): Bitmap {
+  val pixels = toPixelMap()
+  val bitmap = Bitmap.createBitmap(pixels.width, pixels.height, Bitmap.Config.ARGB_8888)
+  bitmap.setPixels(
+    pixels.buffer,
+    pixels.bufferOffset,
+    pixels.stride,
+    0,
+    0,
+    pixels.width,
+    pixels.height
+  )
+  return bitmap
+}
+
+private fun readExistingGolden(testName: String): Bitmap? {
+  return try {
+    ApplicationProvider.getApplicationContext<Context>()
+      .assets
+      .open(testFileName(testName)).use {
+        val options = BitmapFactory.Options()
+        options.inScaled = false
+        BitmapFactory.decodeStream(it, null, options)
+      }
+  } catch (e: FileNotFoundException) {
+    null
+  }
+}
+
+private fun testFileName(testName: String) = "$testName.$EXTENSION"
+
+private fun getTestFilesDir(): File {
+  val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+  return File(dir, GENERATED_FILES_DIR)
+}
+
+private fun writeBitmap(bitmap: Bitmap, testName: String): String {
+  val testFilesDir = getTestFilesDir()
+  require(!(!testFilesDir.exists() && !testFilesDir.mkdirs())) { "Failed to make directory: $testFilesDir" }
+  val file = File(testFilesDir, testFileName(testName))
+  check(!(file.exists() && !file.delete())) { "Failed to remove existing file: $file" }
+  var os: OutputStream? = null
+  try {
+    os = BufferedOutputStream(FileOutputStream(file))
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+    os.close()
+  } catch (e: IOException) {
+    throw RuntimeException(e)
+  } finally {
+    if (os != null) {
+      try {
+        os.close()
+      } catch (e: IOException) {
+        // Ignored.
+      }
+    }
+  }
+  return file.absolutePath
+}
