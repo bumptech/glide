70be029a74a6d262d5a35e2da47238cd2f373e08
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-10-12 16:32:51 +0100

    Proof of concept for investigation of "Cannot recycle a resource while it is still acquired"

--- library/test/src/test/java/com/bumptech/glide/load/engine/ResourceRecyclerTest.java
+++ library/test/src/test/java/com/bumptech/glide/load/engine/ResourceRecyclerTest.java
@@ -2,11 +2,19 @@ package com.bumptech.glide.load.engine;
 
 import static com.bumptech.glide.RobolectricConstants.ROBOLECTRIC_SDK;
 import static com.bumptech.glide.tests.Util.mockResource;
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import android.os.Looper;
+import com.google.common.truth.Correspondence;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -70,5 +78,50 @@ public class ResourceRecyclerTest {
     Shadows.shadowOf(Looper.getMainLooper()).runOneTask();
 
     verify(child).recycle();
+  }
+
+  @Test
+  public void recycle_withChild_asyncTraceable() {
+    final Resource<?> child = mockResource();
+    Throwable someCause = new Throwable("Some simulated cause.");
+    IllegalStateException testEx = new IllegalStateException("Simulated error for test.", someCause);
+    doThrow(testEx).when(child).recycle();
+    Resource<?> parent = mockResource();
+    class ChildRecycler implements Answer<Void> {
+      @Override
+      public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+        recycler.recycle(child, /* forceNextFrame= */ false);
+        return null;
+      }
+    }
+    doAnswer(new ChildRecycler()).when(parent).recycle();
+
+    Shadows.shadowOf(Looper.getMainLooper()).pause();
+    recycler.recycle(parent, /* forceNextFrame= */ false);
+    IllegalStateException ex = assertThrows(
+        IllegalStateException.class,
+        () -> Shadows.shadowOf(Looper.getMainLooper()).runOneTask()
+    );
+    assertSame("Original exception is thrown", testEx, ex);
+    assertSame("Cause is kept", someCause, ex.getCause());
+    assertThat(fullStackOf(ex))
+        .comparingElementsUsing(className())
+        .contains(ChildRecycler.class.getName());
+  }
+
+  private static Correspondence<StackTraceElement, String> className() {
+    return Correspondence.from(
+        (actual, expected) -> actual.getClassName().equals(expected),
+        "StackTraceElement.className"
+    );
+  }
+
+  private static List<StackTraceElement> fullStackOf(Throwable t) {
+    List<StackTraceElement> stack = new ArrayList<>();
+    do {
+      stack.addAll(Arrays.asList(t.getStackTrace()));
+      t = t.getCause();
+    } while (t != null);
+    return stack;
   }
 }
