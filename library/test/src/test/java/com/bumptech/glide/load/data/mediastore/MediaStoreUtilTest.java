46a3b104690cb7cc5f5880e52ecf714d4d89919f
Fares Alhassen <falhassen@google.com>
2024-04-18 19:21:41 -0700

    No public description
    PiperOrigin-RevId: 626222711

---
+++ library/test/src/test/java/com/bumptech/glide/load/data/mediastore/MediaStoreUtilTest.java
@@ -0,0 +1,30 @@
+package com.bumptech.glide.load.data.mediastore;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.net.Uri;
+import android.provider.MediaStore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 18)
+public class MediaStoreUtilTest {
+
+  @Test
+  public void isAndroidPickerUri_notAndroidPickerUri_returnsFalse() {
+    Uri mediaStoreUri = Uri.withAppendedPath(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, "123");
+
+    assertThat(MediaStoreUtil.isAndroidPickerUri(mediaStoreUri)).isFalse();
+  }
+
+  @Test
+  public void isAndroidPickerUri_identifiesAndroidPickerUri_returnsTrue() {
+    Uri androidPickerUri =
+        Uri.parse("content://media/picker/0/com.android.providers.media.photopicker/media/123");
+
+    assertThat(MediaStoreUtil.isAndroidPickerUri(androidPickerUri)).isTrue();
+  }
+}
