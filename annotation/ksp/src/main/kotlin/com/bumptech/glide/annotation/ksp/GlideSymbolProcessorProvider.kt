1723a7141aa5aab9356ee4517f4b0a219adeddca
Donald Duo Zhao <deltazulu@google.com>
2025-03-10 16:21:12 -0700

    No public description
    PiperOrigin-RevId: 735548775

--- annotation/ksp/src/main/kotlin/com/bumptech/glide/annotation/ksp/GlideSymbolProcessorProvider.kt
+++ annotation/ksp/src/main/kotlin/com/bumptech/glide/annotation/ksp/GlideSymbolProcessorProvider.kt
@@ -1,11 +1,11 @@
 package com.bumptech.glide.annotation.ksp
 
-import com.google.auto.service.AutoService
+import com.google.devtools.kotlin.ksp.metainf.MetaInfServices
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
-@AutoService(SymbolProcessorProvider::class)
+@MetaInfServices(implementing = [SymbolProcessorProvider::class])
 class GlideSymbolProcessorProvider : SymbolProcessorProvider {
   override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
     return GlideSymbolProcessor(environment)
