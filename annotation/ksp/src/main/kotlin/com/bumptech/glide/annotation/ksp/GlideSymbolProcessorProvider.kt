1a39132ab49a0d46af302ca5c9820dc63d081ccc
Fares Alhassen <falhassen@google.com>
2025-05-27 12:34:57 -0700

    Rolling back "Migrate `@AutoService` use in Kotlin to `@MetaInfServices`" as it breaks KSP
    PiperOrigin-RevId: 763904215

--- annotation/ksp/src/main/kotlin/com/bumptech/glide/annotation/ksp/GlideSymbolProcessorProvider.kt
+++ annotation/ksp/src/main/kotlin/com/bumptech/glide/annotation/ksp/GlideSymbolProcessorProvider.kt
@@ -1,11 +1,11 @@
 package com.bumptech.glide.annotation.ksp
 
-import com.google.devtools.kotlin.ksp.metainf.MetaInfServices
+import com.google.auto.service.AutoService
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
-@MetaInfServices(implementing = [SymbolProcessorProvider::class])
+@AutoService(SymbolProcessorProvider::class)
 class GlideSymbolProcessorProvider : SymbolProcessorProvider {
   override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
     return GlideSymbolProcessor(environment)
