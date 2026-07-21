cf0cdd51e2e75d3dfb191ae599534152cd20e81d
Fares Alhassen <falhassen@google.com>
2026-07-21 05:47:21 -0700

    Remove path name memoization from Glide disk cache.
    PiperOrigin-RevId: 951436844

--- third_party/disklrucache/build.gradle.kts
+++ third_party/disklrucache/build.gradle.kts
@@ -1,35 +1,32 @@
 plugins {
-    id("com.android.library")
-    checkstyle
+  id("com.android.library")
+  checkstyle
 }
 
 checkstyle {
-    toolVersion = "6.19"
-    configFile = file("checkstyle.xml")
+  toolVersion = "6.19"
+  configFile = file("checkstyle.xml")
 }
 
 android {
-    namespace = "com.bumptech.glide.disklrucache"
-    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+  namespace = "com.bumptech.glide.disklrucache"
+  compileSdk = libs.versions.compile.sdk.version.get().toInt()
 
-    defaultConfig {
-        minSdk = libs.versions.min.sdk.version.get().toInt()
-    }
+  defaultConfig { minSdk = libs.versions.min.sdk.version.get().toInt() }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
 }
 
 dependencies {
-    implementation(libs.androidx.annotation)
-    testImplementation(libs.junit)
-    testImplementation(libs.truth)
+  testImplementation(libs.junit)
+  testImplementation(libs.truth)
 }
 
 val uploaderScript = "${rootProject.projectDir}/scripts/upload.gradle.kts"
 
 if (file(uploaderScript).exists()) {
-    apply(from = "${rootProject.projectDir}/scripts/upload.gradle.kts")
+  apply(from = "${rootProject.projectDir}/scripts/upload.gradle.kts")
 }
